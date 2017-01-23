ddc32977714aa082faafa71698beddaf272d2274
Rangga Adhitya <deneuv3.4@gmail.com>
2017-01-23 21:45:09 +0700

    Some changes to  material guidelines

--- component/timelineComment.js
+++ component/timelineComment.js
@@ -7,18 +7,20 @@ export default class TimelineComment extends Component {
   }
   render() {
     return(
-      <View style={styles.inputContainer}>
-        <TextInput
-          Style={styles.comment}
-          placeholder="Your Comment"
-          onChangeText={(text) => this.setState({text})}
-          multiline={true}
-        />
-        <Button
-          title='Post Comment'
-          color='#009688'
-          accessibilityLabel= 'Post Your Comment'
-        />
+      <View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            Style={styles.comment}
+            placeholder="Your Comment..."
+            onChangeText={(text) => this.setState({text})}
+            multiline={true}
+          />
+          <Button
+            title='Post Comment'
+            color='#009688'
+            accessibilityLabel= 'Post Your Comment'
+          />
+        </View>
       </View>
     )
   }
@@ -26,14 +28,20 @@ export default class TimelineComment extends Component {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex:1,
     borderRadius: 2,
     paddingTop: 16,
-    paddingBottom: 16
+    paddingBottom: 16,
+    margin: 16,
   },
   comment: {
     paddingTop: 16,
     borderWidth: 1,
     paddingBottom: 16,
   },
+  button: {
+    borderRadius: 2,
+    height:32,
+    margin: 16,
+    fontSize: 13,
+  }
 })
