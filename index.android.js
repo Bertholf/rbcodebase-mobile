b3bc2975f75c412e84278152fefb791d274798be
Rangga Adhitya <deneuv3.4@gmail.com>
2017-01-23 16:55:33 +0700

    fix index

--- index.android.js
+++ index.android.js
@@ -18,11 +18,121 @@ import { strings } from './app/lib/Locale';
 
 import Timeline from './Timeline'
 
+
 class ReactProject extends Component {
   render() {
-    return(
-      <Timeline />
-    )
+    var initialRoute = { name:'bridge', title: strings.signup }
+    return (
+      <Navigator
+        initialRoute={initialRoute}
+        renderScene={RouteMapper}
+        ref={(nav) => { this.navigator = nav }}
+        />
+    );
+  }
+
+  componentDidMount() {
+    BackAndroid.addEventListener('hardwareBackPress', this.onBackPress.bind(this));
+  }
+
+  onBackPress() {
+    if (this.navigator && this.navigator.getCurrentRoutes().length > 1) {
+        this.navigator.pop();
+        return true;
+    }
+    return false;
+  }
+}
+
+var RouteMapper = function(route, navigator) {
+  switch (route.name) {
+    case 'pin':
+      return (
+        <Pin
+          title={route.title}
+          navigator={navigator}
+          />
+      );
+      break;
+    case 'login':
+      return (
+        <Login
+          navigator={navigator}
+          />
+      );
+      break;
+    case 'bridge':
+      return (
+        <BridgetList
+          navigator={navigator}
+          />
+        );
+      break
+    case 'ds':
+      return (
+        <DeviceState
+          navigator={navigator}
+          />
+        );
+      break;
+    case 'sp':
+      return (
+        <SharedPrefs
+          navigator={navigator}
+          />
+        );
+      break;
+    case 'rest':
+      return (
+        <Rest
+          navigator={navigator}
+          />
+        );
+      break;
+    case 'contact':
+      return (
+        <Contact
+          navigator={navigator}
+          />
+        );
+      break;
+    case 'notif':
+      return (
+        <Notif
+          navigator={navigator}
+          />
+        );
+      break;
+    case 'receiver':
+      return (
+        <Receiver
+          navigator={navigator}
+          />
+        );
+      break;
+    case 'service':
+      return (
+        <Service
+          navigator={navigator}
+          />
+        );
+      break;
+    case 'sms':
+      return (
+        <Sms
+          navigator={navigator}
+          />
+        );
+      break;
+    case 'db':
+      return (
+        <Database
+          navigator={navigator}
+          />
+        );
+      break;
+    default:
+      break;
   }
 }
 
