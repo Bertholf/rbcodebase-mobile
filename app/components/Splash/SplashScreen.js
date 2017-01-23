a0e8740733644ce57c341c8e181a6254a0db496f
Muhammad Syamsul <msyamsulanwr@gmail.com>
2017-01-23 19:00:58 +0700

    modified splashScreen

--- app/components/Splash/SplashScreen.js
+++ app/components/Splash/SplashScreen.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 const SplashScreen = (props) => (
-  <View style={{ flex: 1, flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Hello World</Text>
+  <View style={{ flex: 1, flexGrow: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#009688' }}>
+    <Image source={{uri: 'https://s-media-cache-ak0.pinimg.com/736x/60/15/4f/60154f09e46dcf77148ccae0717963db.jpg'}}
+        style={{
+          width: 250,
+          height: 250,
+          borderRadius: 125,
+          borderWidth: 2,
+          borderColor: '#009688'
+        }}/>
+    <Text style={{
+      fontSize: 30,
+    }}>Hiekr</Text>
   </View>
 );
 export default SplashScreen;
