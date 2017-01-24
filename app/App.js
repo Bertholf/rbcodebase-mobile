f1d570c44c22e013d137ed3992a947a63e5f6b1a
Mochamad Gufron <mgufronefendi@gmail.com>
2017-01-24 10:41:35 +0700

    add redux persist

--- app/App.js
+++ app/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { routerReducer } from 'react-native-redux-router';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
+import {persistStore, autoRehydrate} from 'redux-persist'
 import todoApp from './reducers';
 import Routing from './containers/Router';
 
 const logger = createLogger();
 const store = createStore(combineReducers({ routerReducer, todoApp }),
-  applyMiddleware(logger, thunkMiddleware));
+  applyMiddleware(logger, thunkMiddleware),
+  autoRehydrate());
+persistStore(store, {
+  storage: AsyncStorage,
+});
+
 const App = () => (<Provider store={store}>
   <Routing />
 </Provider>);
