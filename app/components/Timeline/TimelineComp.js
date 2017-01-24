a54b3b392013a8d105f9ced70708180b8f585b94
shilohchis <shidei.chis@gmail.com>
2017-01-24 16:26:32 +0700

    Revision: Adding List View

--- app/components/Timeline/TimelineComp.js
+++ app/components/Timeline/TimelineComp.js
@@ -1,62 +1,82 @@
 import React, { Component } from 'react';
 import {
+  AppRegistry,
   StyleSheet,
   Text,
   View,
   Image,
+  Navigator,
+  ListView
 } from 'react-native';
-
-const styles = StyleSheet.create({
-  card: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    backgroundColor: '#009688',
-
-  },
-  text: {
-    fontSize: 10,
-    textAlign: 'right',
-    margin: 2,
-    color: '#f8f8ff',
-    backgroundColor: '#009688',
-
-    padding: 8,
-    elevation: 3,
-  },
-  nameProfile: {
-    fontWeight: 'bold',
-    textAlign: 'left',
-    color: '#ffffff',
-    padding: 8,
-    fontSize: 30,
-    margin: 4,
-    backgroundColor: '#009688',
-  },
-  mainView: {
-    marginLeft: 16,
-    marginRight: 16,
-
-    backgroundColor: '#ffffff',
-    elevation: 4,
-  },
-});
-
+import TimelineComment from './timelineComment';
 export default class MapMain extends Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+      ])
+    };
+  }
   render() {
     return (
+      <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) =>
+
       <View style={styles.mainView}>
-        <View style={styles.card}>
-          <Image source={require('./1.jpeg')} style={{ width: 75, height: 75, borderRadius: 70, margin: 6 }} />
-          <Text style={styles.nameProfile}>Nama Profile</Text>
-        </View>
+       <View style={styles.card}>
+         <Image source = {require('./1.jpeg')} style = {{width: 75, height: 75, borderRadius:70, margin:6}}/>
+         <Text style={styles.nameProfile}>{rowData}</Text>
+       </View>
 
-        <Image source={{ uri: 'http://ke5ter.com/img/route.png' }} style={{ width: 325, height: 183, alignItems: 'center', marginLeft: 2 }} />
+         <Image source = {{uri:'http://ke5ter.com/img/route.png'}} style = {{width: 325, height: 183, justifyContent: 'center', marginLeft: 9 }}/>
 
-        <Text style={styles.text}>Tangkuban Perahu</Text>
-        <Text style={styles.text}>KM</Text>
+          <Text style={styles.text}>Tangkuban Perahu</Text>
+          <Text style={styles.text}>KM</Text>
+          <TimelineComment></TimelineComment>
+    </View>
 
-      </View>
 
+
+}
+/>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+
+    flexDirection:'row',
+    justifyContent:'flex-start',
+    backgroundColor: '#009688',
+
+  },
+
+  text: {
+    fontSize: 10,
+    textAlign: 'right',
+    paddingTop:6,
+    color:'#f8f8ff',
+    backgroundColor: '#009688',
+    padding:8,
+    marginTop:2
+
+  },
+  nameProfile: {
+    fontWeight:'bold',
+    textAlign: 'left',
+    color: '#ffffff',
+    padding: 8,
+    fontSize:30,
+
+    backgroundColor: '#009688'
+  },
+  mainView:{
+
+    backgroundColor:'#ffffff',
+
+  }
+});
