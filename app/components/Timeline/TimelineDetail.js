6d065e708b122f635451f8cf868973c0f6a7c587
ahmadahmadi14 <ahmadahmadi140989@gmail.com>
2017-01-25 15:07:35 +0700

    update button reset, zoom

--- app/components/Timeline/TimelineDetail.js
+++ app/components/Timeline/TimelineDetail.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Image,
   TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -26,10 +27,10 @@ const styles = StyleSheet.create({
     fontSize: 10,
   },
   img: {
-    width: 250,
+    width: 300,
     height: 250,
     margin: 15,
-    marginLeft: 50,
+    marginLeft: 20,
   },
   detail: {
     marginRight: 15,
@@ -42,37 +43,45 @@ const styles = StyleSheet.create({
     padding: 5,
     borderColor: 'gray',
   },
+  container: {
+    flex: 1,
+  },
 });
 
 export default class CardDetail extends Component {
   render() {
     return (
       <ScrollView>
-      <View style={{ marginLeft: 8, marginRight: 8, marginTop: 15, backgroundColor: 'white', elevation: 12 }}>
-        <Text style={styles.fntui}> Detail card </Text>
-        <View style={{ flexDirection: 'row', marginLeft: 15, marginTop: 10 }}>
-          <Image style={{ borderRadius: 70, width: 50, height: 50 }} source={{ uri: 'http://facebook.github.io/react/img/logo_og.png' }} />
-          <View style={{ flexDirection: 'column', marginLeft: 15 }}>
-            <Text style={styles.user}>User name</Text>
-            <Text style={styles.time}>Date/time</Text>
+        <View style={{ marginLeft: 8, marginRight: 8, marginTop: 15, backgroundColor: 'white', elevation: 12 }}>
+          <Text style={styles.fntui}> Detail card </Text>
+          <View style={{ flexDirection: 'row', marginLeft: 15, marginTop: 10 }}>
+            <Image style={{ borderRadius: 70, width: 50, height: 50 }} source={{ uri: 'http://facebook.github.io/react/img/logo_og.png' }} />
+            <View style={{ flexDirection: 'column', marginLeft: 15 }}>
+              <Text style={styles.user}>User name</Text>
+              <Text style={styles.time}>Date/time</Text>
+            </View>
+          </View>
+          <View>
+            <Image source={{ uri: 'http://facebook.github.io/react/img/logo_og.png' }} style={styles.img} />
+          </View>
+          <View style={{ flexDirection: 'column', justifyContent: 'center',height: 100 }}>
+             <TouchableOpacity <Image source={require('../../images/reset.png')} style={{width: 30, height: 30 }} /> />
+            <TouchableOpacity <Image source={require('../../images/zoom in.png')} style={{width: 30, height: 30 }} /> />
+            <TouchableOpacity <Image source={require('../../images/zoom out.png')} style={{width: 30, height: 30 }} /> />
+          </View>
+          <View>
+            <Text style={styles.detail}>Detail:ini  adalah detail ini adalah detail</Text>
+            <Text>{'\n'}</Text>
+          </View>
+          <View>
+            <Text style={styles.comment}>Comment</Text>
+            <TextInput
+              style={styles.comment}
+              onChangeText={text => this.setState({ text })}
+            />
           </View>
         </View>
-        <View>
-          <Image source={{ uri: 'http://facebook.github.io/react/img/logo_og.png' }} style={styles.img} />
-        </View>
-        <View>
-          <Text style={styles.detail}>Detail:ini  adalah detail ini adalah detail</Text>
-          <Text>{'\n'}</Text>
-        </View>
-        <View>
-          <Text style={styles.comment}>Comment</Text>
-          <TextInput
-            style={styles.comment}
-            onChangeText={text => this.setState({ text })}
-          />
-        </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
     );
   }
   }
