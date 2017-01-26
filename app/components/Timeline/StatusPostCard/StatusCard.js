bd672429bbf93281149967be06dd3c220a73636b
Rangga Adhitya <deneuv3.4@gmail.com>
2017-01-26 18:59:34 +0700

    add button and some icons

--- app/components/Timeline/StatusPostCard/StatusCard.js
+++ app/components/Timeline/StatusPostCard/StatusCard.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, Image, Button } from 'react-native';
 import styles from './../../../components/Timeline/StatusPostCard/styles';
 
 const PostCard = () => {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>What's Going on</Text>
-      </View>
-      <View>
-        <TextInput
-          multiline={true}
-          blurOnSubmit={true}
-        />
-      </View>
-      <View>
-        <Text>This is Bottom</Text>
+    <View style={{ backgroundColor: '#BDBDBD' }}>
+      <View style={styles.containerCard}>
+        <Text style={styles.titleText}>What's Going On</Text>
+        <View>
+          <TextInput
+            multiline={true}
+            blurOnSubmit={true}
+            placeholder="Your Status"
+            placeholderTextColor="#BDBDBD"
+            underlineColorAndroid="#64B5F6"
+          />
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={styles.containerBottom}>
+            <View style={styles.image}>
+              <Image
+                source={require('./../../../images/ic_perm_identity_black_24dp.png')}
+                style={styles.image}
+              />
+              <Image
+                source={require('./../../../images/ic_place_black_24dp.png')}
+                style={styles.image}
+              />
+              <Image
+                source={require('./../../../images/camera.png')}
+                style={styles.image}
+              />
+              <Image
+                source={require('./../../../images/ic_insert_emoticon_black_24dp.png')}
+                style={styles.image}
+              />
+            </View>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Button title={'Post'} />
+          </View>
+        </View>
       </View>
     </View>
   );
--- app/components/Timeline/StatusPostCard/styles.js
+++ app/components/Timeline/StatusPostCard/styles.js
@@ -1,9 +1,29 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  containerCard: {
     margin: 20,
-  }
+    padding: 14,
+    backgroundColor: '#ffffff',
+    borderRadius: 2,
+    elevation: 2,
+  },
+  titleText: {
+    color: '#757575',
+    fontWeight: 'bold',
+    fontSize: 17,
+  },
+  containerBottom: {
+    flexDirection: 'row',
+    flex: 3,
+  },
+  image: {
+    paddingTop: 10,
+    paddingBottom: 14,
+    height: 24,
+    width: 24,
+    flexDirection: 'row',
+  },
 });
 
 module.exports = styles;
