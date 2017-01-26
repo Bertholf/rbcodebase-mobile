2c3f2c091024cf8f5ed248023257d1954086b9d4
putraranji <ogidoank51@yahoo.com>
2017-01-26 12:28:32 +0700

    Modified ListTrails.js and add Bar Chart

--- app/components/ListTrail/ListTrails.js
+++ app/components/ListTrail/ListTrails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View, Image, ListView, Dimensions } from 'react-native';
+import { Text, StyleSheet, View, Image, Dimensions, ScrollView } from 'react-native';
+import { BarChart } from 'react-native-charts';
 
 const imageMenu = require('./../../images/menu.png');
 const accessible = require('./../../images/accessible.png');
@@ -9,6 +10,8 @@ const search = require('./../../images/search.png');
 const morepert = require('./../../images/morepert.png');
 const arrow = require('./../../images/arrow.png');
 const image = require('./../../images/mountain.jpg');
+const kelimutu = require('./../../images/kelimutu.jpg');
+const rinjani = require('./../../images/rinjani.jpg');
 
 const { height, width } = Dimensions.get('window');
 const styles = StyleSheet.create({
@@ -22,12 +25,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#009688',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    elevation: 8,
   },
   imgMenu: {
     width: 30,
     height: 30,
     top: 15,
-    left: 15,
+    left: 10,
   },
   menubar: {
     width: width * 0.8,
@@ -51,28 +55,47 @@ const styles = StyleSheet.create({
   listTrail: {
     flexDirection: 'row',
     top: 5,
-    justifyContent: 'center',
+    marginLeft: 5,
   },
   menupert: {
     width: 25,
     height: 25,
   },
   list: {
-    flexDirection: 'row',
     backgroundColor: '#00BFA5',
-    justifyContent: 'space-between',
-    width: width * 0.93,
+    flexDirection: 'row',
+    height: 50,
   },
-  text: {
+  title: {
     color: '#FFFFFF',
+    fontSize: 18,
+    margin: 5,
+  },
+  sub: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    margin: 5,
   },
   row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  label: {
+    backgroundColor: '#00BFA5',
+    paddingLeft: 3,
+    paddingRight: 3,
+    marginRight: 5,
+  },
+  chart: {
+    width: 200,
+    height: 200,
   },
 });
+
 export default class ListTrails extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.container} >
         <View style={styles.header}>
           <Image style={styles.imgMenu} source={imageMenu} />
           <View style={styles.menubar}>
@@ -84,19 +107,154 @@ export default class ListTrails extends Component {
             <Image style={styles.menuBtn} source={morepert} />
           </View>
         </View>
-        <View style={styles.listTrail} >
-          <Image style={styles.arrow} source={arrow} />
-          <View style={styles.row} >
-            <View style={styles.list}>
-              <Image style={styles.imageSide} source={image} />
-              <Text style={styles.text}>Longmire</Text>
-              <Text style={styles.text}>S8511.02mi</Text>
-              <Image style={styles.menupert} source={morepert} />
-              <Text style={styles.text}>On Trail</Text>
-              <Text style={styles.text}>WT mi-89</Text>
+        <ScrollView style={styles.scroll}>
+          <View style={styles.listTrail} >
+            <Image style={styles.arrow} source={arrow} />
+            <Image style={styles.imageSide} source={image} />
+            <View style={styles.label}>
+              <View style={styles.row}>
+                <Text style={styles.title}>Longmire</Text>
+                <Text style={styles.title}>S8511.02mi</Text>
+                <Image style={styles.menupert} source={morepert} />
+              </View>
+              <Text style={styles.sub}>On Trail</Text>
+              <Text style={styles.sub}>WT mi-89</Text>
             </View>
           </View>
-        </View>
+          <View style={{ height: 5 }} />
+          <View style={styles.listTrail} >
+            <Image style={styles.arrow} source={arrow} />
+            <BarChart
+              dataSets={[
+                {
+                  fillColor: '#46b3f7',
+                  data: [
+                      { value: 15 },
+                      { value: 10 },
+                      { value: 12 },
+                      { value: 11 },
+                  ],
+                },
+                {
+                  fillColor: '#3386b9',
+                  data: [
+                        { value: 14 },
+                        { value: 11 },
+                        { value: 14 },
+                        { value: 13 },
+                  ],
+                },
+              ]}
+              graduation={1}
+              horizontal={false}
+              howGrid={true}
+              barSpacing={5}
+              style={{
+                height: 100,
+                margin: 15,
+                width: 300,
+              }}
+              />
+          </View>
+          <View style={{ height: 5 }} />
+          <View style={styles.listTrail} >
+            <Image style={styles.arrow} source={arrow} />
+            <Image style={styles.imageSide} source={kelimutu} />
+            <View style={styles.label}>
+              <View style={styles.row}>
+                <Text style={styles.title}>Mowich</Text>
+                <Text style={styles.title}>N839.60mi</Text>
+                <Image style={styles.menupert} source={morepert} />
+              </View>
+              <Text style={styles.sub}>On Trail</Text>
+              <Text style={styles.sub}>WT mi-33</Text>
+            </View>
+          </View>
+          <View style={{ height: 5 }} />
+          <View style={styles.listTrail} >
+            <Image style={styles.arrow} source={arrow} />
+            <BarChart
+              dataSets={[
+                {
+                  fillColor: '#F44336',
+                  data: [
+                      { value: 7 },
+                      { value: 5 },
+                      { value: 9 },
+                      { value: 6 },
+                  ],
+                },
+                {
+                  fillColor: '#1A237E',
+                  data: [
+                        { value: 10 },
+                        { value: 9 },
+                        { value: 7 },
+                        { value: 8 },
+                  ],
+                },
+              ]}
+              graduation={1}
+              horizontal={false}
+              howGrid={true}
+              barSpacing={5}
+              style={{
+                height: 100,
+                margin: 15,
+                width: 300,
+              }}
+              />
+          </View>
+          <View style={{ height: 5 }} />
+          <View style={styles.listTrail} >
+            <Image style={styles.arrow} source={arrow} />
+            <Image style={styles.imageSide} source={rinjani} />
+            <View style={styles.label}>
+              <View style={styles.row}>
+                <Text style={styles.title}>Sunrise</Text>
+                <Text style={styles.title}>8460.17mi</Text>
+                <Image style={styles.menupert} source={morepert} />
+              </View>
+              <Text style={styles.sub}>On Trail</Text>
+              <Text style={styles.sub}>WT mi 54</Text>
+            </View>
+          </View>
+          <View style={{ height: 5 }} />
+          <View style={styles.listTrail} >
+            <Image style={styles.arrow} source={arrow} />
+            <BarChart
+              dataSets={[
+                {
+                  fillColor: '#C51162',
+                  data: [
+                      { value: 3 },
+                      { value: 7 },
+                      { value: 2 },
+                      { value: 11 },
+                  ],
+                },
+                {
+                  fillColor: '#6A1B9A',
+                  data: [
+                        { value: 4 },
+                        { value: 6 },
+                        { value: 10 },
+                        { value: 5 },
+                  ],
+                },
+              ]}
+              graduation={1}
+              horizontal={false}
+              howGrid={true}
+              barSpacing={5}
+              style={{
+                height: 100,
+                margin: 15,
+                width: 300,
+              }}
+              />
+          </View>
+        </ScrollView>
       </View>
     );
   }
