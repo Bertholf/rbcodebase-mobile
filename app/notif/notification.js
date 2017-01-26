712f962f2ae0797d197b30041eb31a96a13c8b80
ahmadahmadi14 <ahmadahmadi140989@gmail.com>
2017-01-26 22:50:22 +0700

    eslint revisition style

--- app/notif/notification.js
+++ app/notif/notification.js
@@ -5,52 +5,41 @@ import {
   Text,
   View,
   Image,
-  Navigator,
   ListView,
 } from 'react-native';
 
 const styles = StyleSheet.create({
   card: {
-    flexDirection:'row',
-    justifyContent:'flex-start',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
     backgroundColor: '#42a5f5',
     borderColor: '#ffffff',
-    borderWidth: 1,
-    borderColor: 'white',
-  },
-  text: {
-    fontSize: 1,
-    textAlign: 'right',
-    paddingTop:6,
-    color:'#42a5f5',
-    backgroundColor: '#ffffff',
-    padding:8,
-    marginTop:2,
+    borderWidth: 2,
   },
   nameProfile: {
-    fontWeight:'bold',
+    fontWeight: 'bold',
     textAlign: 'left',
     color: '#ffffff',
     padding: 8,
-    fontSize:20,
+    fontSize: 20,
     backgroundColor: '#42a5f5',
   },
-  mainView:{
-    backgroundColor:'#ffffff',
+  mainView: {
+    backgroundColor: '#ffffff',
   },
-  square : {
-    width: 50,
-    height: 50 ,
-    marginLeft : 10,
-    marginTop : 10,
-  }
+  square: {
+    width: 60,
+    height: 60,
+    marginLeft: 10,
+    marginTop: 10,
+  },
 });
 
 export default class Notification extends Component {
   // Initialize the hardcoded data
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
       dataSource: ds.cloneWithRows([
         'John',
@@ -61,25 +50,24 @@ export default class Notification extends Component {
         'Jillian',
         'Julie',
         'Devin',
-      ])
+      ]),
     };
   }
-    renderRow(rowData) {
+  renderRow(rowData) {
     return (
-      <TouchableOpacity style={styles.mainView} onPress={()=>props.moveToDetail()}>
+      <TouchableOpacity style={styles.mainView}>
         <View style={styles.card}>
           <Image style={styles.square} source={{ uri: 'http://facebook.github.io/react/img/logo_og.png' }} />
-          <View style={{ flexDirection: 'column', marginLeft: 15 }}>
-          <Text style={styles.nameProfile}>{rowData}</Text>
+          <View style={{ flexDirection: 'column', marginLeft: 12 }}>
+            <Text style={styles.nameProfile}>{rowData}</Text>
             <Text style={styles.time}>this is a detail detail detail</Text>
             <Text style={styles.time}>Date/time</Text>
-       </View>
-
+          </View>
         </View>
       </TouchableOpacity>
     );
-}
-render() {
+  }
+  render() {
     return (
       <ListView
         dataSource={this.state.dataSource}
