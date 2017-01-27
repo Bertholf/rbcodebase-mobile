92d54e4f94b569eca81cc39323b224ca044c3bdc
Prayudi Tirta Bayu <prayuditb@gmail.com>
2017-01-27 14:42:16 +0700

    add gender button

--- app/components/Auth/register-new.js
+++ app/components/Auth/register-new.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   View,
+  ScrollView,
   TextInput,
   TouchableOpacity,
   Dimensions,
@@ -11,16 +12,17 @@ import {
   Picker,
 } from 'react-native';
 
-const { width, height } = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 const logo = require('./../../images/RegisterScreen/user.png');
 const google = require('./../../images/google-plus.png');
 const facebook = require('./../../images/facebook.png');
 const twitter = require('./../../images/twitter.png');
+const male = require('./../../images/male.png');
+const female = require('./../../images/female.png');
 
 const styles = StyleSheet.create({
   container: {
     width,
-    height,
     alignItems: 'center',
   },
   logo: {
@@ -39,7 +41,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#2196f3',
     padding: 8,
     borderRadius: 3,
-    bottom: 10,
+    shadowOffset: { width: 15, height: 15 },
+    shadowColor: '#000',
   },
   textReg: {
     color: '#fff',
@@ -48,7 +51,7 @@ const styles = StyleSheet.create({
   },
   buttonGroup: {
     top: 10,
-    width: width * 0.8,
+    width: width * 0.6,
     height: 60,
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -58,8 +61,12 @@ const styles = StyleSheet.create({
     width: 50,
     alignSelf: 'stretch',
   },
+  separator: {
+    borderBottomWidth: 1,
+    borderColor: '#f0f0f0',
+  },
   separatorText: {
-    fontSize: 18,
+    fontSize: 15,
   },
   picker: {
     height: 50,
@@ -67,7 +74,26 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#f0f0f0',
   },
-
+  genderRow: {
+    width: width * 0.9,
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginBottom: 7,
+  },
+  btnGender: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    height: 55,
+    width: 55,
+    marginRight: 10,
+    borderWidth: 1,
+  },
+  imgGender: {
+    height: 40,
+    width: 40,
+  },
 });
 
 const Register = () => {
@@ -75,59 +101,62 @@ const Register = () => {
   const register = () => {
     Alert.alert('Button pressed');
   };
+  let selected = false;
   return (
-    <View style={styles.container}>
-      <Image style={styles.logo} source={logo} />
-      <TextInput
-        placeholder={'Name'}
-        style={styles.textInput}
-        onChangeText={name => console.log({ name })}
-      />
-      <TextInput
-        placeholder={'Email'}
-        style={styles.textInput}
-        onChangeText={email => console.log({ email })}
-      />
-      <TextInput
-        placeholder={'Username'}
-        style={styles.textInput}
-        onChangeText={username => console.log({ username })}
-      />
-      <View style={{ alignItems: 'flex-start', width: width * 0.87, height: 20 }} >
-        <Text>Gender</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image style={styles.logo} source={logo} />
+        <TextInput
+          placeholder={'Name'}
+          style={styles.textInput}
+          onChangeText={name => console.log({ name })}
+        />
+        <TextInput
+          placeholder={'Email'}
+          style={styles.textInput}
+          onChangeText={email => console.log({ email })}
+        />
+        <TextInput
+          placeholder={'Username'}
+          style={styles.textInput}
+          onChangeText={username => console.log({ username })}
+        />
+        <TextInput
+          placeholder={'Password'}
+          style={styles.textInput}
+          onChangeText={password => console.log({ password })}
+        />
+        <View style={{ alignItems: 'flex-start', width: width * 0.87, height: 20 }} >
+          <Text>Gender</Text>
+        </View>
+        <View style={styles.genderRow} >
+          <TouchableOpacity style={styles.btnGender} >
+            <Image source={male} style={styles.imgGender} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnGender}>
+            <Image source={female} style={styles.imgGender} />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          style={styles.btnReg}
+          onPress={register}
+        >
+          <Text style={styles.textReg}>Register</Text>
+        </TouchableOpacity>
+        <Text style={styles.separatorText}>Register with:</Text>
+        <View style={styles.buttonGroup}>
+          <TouchableOpacity onPress={register} >
+            <Image source={google} style={styles.icon} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={register} >
+            <Image source={facebook} style={styles.icon} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={register}>
+            <Image source={twitter} style={styles.icon} />
+          </TouchableOpacity>
+        </View>
       </View>
-      <Picker
-        style={styles.picker}
-        selectedValue={''}
-        onValueChange={gender => console.log({ gender })}
-      >
-        <Picker.Item label={'Male'} value={'male'} />
-        <Picker.Item label={'Female'} value={'female'} />
-      </Picker>
-      <TextInput
-        placeholder={'Password'}
-        style={styles.textInput}
-        onChangeText={password => console.log({ password })}
-      />
-      <TouchableOpacity
-        style={styles.btnReg}
-        onPress={register}
-      >
-        <Text style={styles.textReg}>Register</Text>
-      </TouchableOpacity>
-      <Text style={styles.separatorText}>Register with:</Text>
-      <View style={styles.buttonGroup}>
-        <TouchableOpacity onPress={register} >
-          <Image source={google} style={styles.icon} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={register} >
-          <Image source={facebook} style={styles.icon} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={register}>
-          <Image source={twitter} style={styles.icon} />
-        </TouchableOpacity>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
