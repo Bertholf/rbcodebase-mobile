0fcb826398c2f96035a38fcacd58881a2c3ccd2c
Muhammad Syamsul <msyamsulanwr@gmail.com>
2017-01-27 15:08:45 +0700

    change style to separate file

--- app/components/Setting/Setting.js
+++ app/components/Setting/Setting.js
@@ -1,41 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2196f3',
-  },
-  imagesLeft: {
-    width: 25,
-    height: 25,
-    marginRight: 15,
-  },
-  list: {
-    marginTop: 10,
-    alignItems : 'center',
-    flexDirection: 'row',
-    padding: 15,
-    backgroundColor: '#ffffff',
-  },
-  list1: {
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    margin: 20,
-    padding: 15,
-  },
-  text: {
-    color: '#000000',
-    fontSize: 20,
-  },
-  deactive: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: 'bold',
-    backgroundColor: 'red',
-    padding: 10,
-  },
-})
+import styles from './../../components/Setting/Style';
 
 const Setting = () => {
     return (
