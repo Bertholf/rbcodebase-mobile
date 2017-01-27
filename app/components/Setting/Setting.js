a4d6d3b86c205c2bfe8a429e9fb5fba1ea9bd5e1
Muhammad Syamsul <msyamsulanwr@gmail.com>
2017-01-27 11:19:11 +0700

    modified SettingScreen

--- app/components/Setting/Setting.js
+++ app/components/Setting/Setting.js
@@ -1,5 +1,41 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2196f3',
+  },
+  imagesLeft: {
+    width: 25,
+    height: 25,
+    marginRight: 15,
+  },
+  list: {
+    marginTop: 10,
+    alignItems : 'center',
+    flexDirection: 'row',
+    padding: 15,
+    backgroundColor: '#ffffff',
+  },
+  list1: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    margin: 20,
+    padding: 15,
+  },
+  text: {
+    color: '#000000',
+    fontSize: 20,
+  },
+  deactive: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    backgroundColor: 'red',
+    padding: 10,
+  },
+})
 
 export default class Setting extends Component {
   render() {
@@ -23,38 +59,12 @@ export default class Setting extends Component {
             <Text style={styles.text}>Email</Text>
           </View>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <View style={styles.list1}>
-            <Text style={styles.text}>Deactive</Text>
-          </View>
-        </TouchableOpacity>
+        <View style={styles.list1}>
+          <TouchableOpacity>
+              <Text style={styles.deactive}>Deactive</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 10,
-    backgroundColor: 'grey',
-  },
-  imagesLeft: {
-    marginRight: 15,
-  },
-  list: {
-    margin: 5,
-    alignItems : 'center',
-    flexDirection: 'row',
-    padding: 15,
-    backgroundColor: 'white',
-  },
-  list1: {
-    margin: 5,
-    alignItems: 'center',
-    padding: 15,
-    backgroundColor: 'red',
-  },
-  text: {
-    fontSize: 20,
-  }
-})
