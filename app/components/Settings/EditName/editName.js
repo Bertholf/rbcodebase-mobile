2dea35843e120ecc77643a21e23ab5a52e3e503d
Rangga Adhitya <deneuv3.4@gmail.com>
2017-01-27 15:02:22 +0700

    add more styling

--- app/components/Settings/EditName/editName.js
+++ app/components/Settings/EditName/editName.js
@@ -10,21 +10,28 @@ import styles from './../../../components/Settings/EditName/style';
 const UpdateName = () => {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>First Name</Text>
+      <View style={{ alignItems: 'center', marginBottom: 50 }}>
+        <Text style={{ fontSize: 32, color: '#757575' }}>Change Name</Text>
+      </View>
+      <View style={styles.inputMargin}>
+        <View>
+          <Text style={styles.title}>First Name</Text>
+        </View>
+        <View>
+          <TextInput
+            placeholder={'First Name'}
+            placeholderTextColor="#BDBDBD"
+          />
+        </View>
       </View>
       <View>
-        <TextInput
-          placeholder={'First Name'}
-        />
-      </View>
-      <View>
-        <Text>Last Name</Text>
+        <Text style={styles.title}>Last Name</Text>
         <TextInput
           placeholder={'Last Name'}
+          placeholderTextColor="#BDBDBD"
         />
       </View>
-      <View>
+      <View style={styles.button}>
         <Button
           title={'Save'}
         />
---
+++ app/components/Settings/EditName/style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 20,
+    paddingLeft: 14,
+    paddingRight: 14,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 12,
+    color: '#212121',
+  },
+  button: {
+    marginTop: 34,
+  },
+  inputMargin: {
+    marginBottom: 20,
+    justifyContent: 'flex-start',
+  },
+});
+
+module.exports = styles;
