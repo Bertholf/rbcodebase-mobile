58bb71f1d784798b0d1c6b2f1ddc08fdb75a6d26
Sutani Ghifari <sutanighifari@gmail.com>
2017-01-29 17:42:41 +0700

    update index.android

--- index.android.js
+++ index.android.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {
   AppRegistry,
 } from 'react-native';
-import Profile from './app/components/Profile/Profile'
+import App from './app/App'
 
 const HikerApp = () => (
-  <Profile />
+  <App />
 );
 AppRegistry.registerComponent('hikerapp', () => HikerApp);
