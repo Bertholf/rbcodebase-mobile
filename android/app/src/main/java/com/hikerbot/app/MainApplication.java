c0f1b40ccbd576f7709292b6e973fc57d2e02a44
Prayudi Tirta Bayu <prayuditb@gmail.com>
2017-01-29 20:40:24 +0700

    install and link fbsdk

--- android/app/src/main/java/com/hikerbot/app/MainApplication.java
+++ android/app/src/main/java/com/hikerbot/app/MainApplication.java
@@ -3,6 +3,7 @@ package com.hikerbot.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.mapbox.reactnativemapboxgl.ReactNativeMapboxGLPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(),
             new ReactMaterialKitPackage(),
             new MapsPackage(),
             new ReactNativeMapboxGLPackage(),
