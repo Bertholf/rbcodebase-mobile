79ef3a7ffb5883dd2340fc38091e29f669602e5e
awebr000 <mas.awe.awe@gmail.com>
2017-01-30 15:16:38 +0700

    make mockup comment service

---
+++ app/services/comment.js
@@ -0,0 +1,12 @@
+import api from './api';
+
+const comment = {
+  getComments: (postId) => api.get('/post/'+postId+'/comments'),
+  newComment: (postId, data) => api.post('/post/'+postId+'/comments', data),
+  updateComment: (postId, commentId, data) => api.put('/post/'+postId+'/comments/'+commentId, data),
+  deleteComment: (postId, commentId) => api.delete('/post/'+postId+'/comments/'+commentId),
+  likeComment: (postId, commentId) => api.put('/post/'+postId+'/comments/'+commentId),
+  unlikeComment: (postId, commentId) => api.put('/post/'+postId+'/comments/'+commentId),
+};
+
+export default comment;
