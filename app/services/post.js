0dffc166c8732ca8497ea07535a3a322e7687766
awebr000 <mas.awe.awe@gmail.com>
2017-01-31 18:12:14 +0700

    create pos service method

--- app/services/post.js
+++ app/services/post.js
@@ -1,5 +1,12 @@
 import api from './api';
 
-export default {
-  getAllPosts: () => api.get('/posts'),
-};
+const post = {
+  getPosts: () => api.get('/posts'),
+  updatePost: (id, data) => api.put('/posts/'+id, data),
+  deletePost: (id) => api.delete('/posts'),
+  newPost: (data) => api.post('/posts', data),
+  likePost: (id) => api.put('/posts/'+id+'/like'),
+  unlikePost: (id) => api.put('/posts/'+id+'/unlike')
+}
+
+export default post
