cb6f734ce2ac160ebf0e45ec1079412fb6e9b76c
Aristyo Hadikusuma <tyoaris21@gmail.com>
2017-01-31 12:11:28 +0700

    conflict index.android.js

--- index.android.js
+++ index.android.js
@@ -5,31 +5,13 @@
  * @flow
  */
 
-// import React from 'react';
-// import {
-//   AppRegistry,
-// } from 'react-native';
-// import App from './app/App';
-//
-// const HikerApp = () => (
-//   <App />
-// );
-// AppRegistry.registerComponent('hikerapp', () => HikerApp);
-
-
-import React, {Component} from 'react';
+import React from 'react';
 import {
   AppRegistry,
-  View,
 } from 'react-native';
-import Chat from './app/components/Chat/ChatView';
+import App from './app/App';
 
-export default class HikerApp extends Component {
-render() {
-  return (
-    <View><Chat/></View>
-  );
-}
-}
-
-AppRegistry.registerComponent('hikerapp', () => HikerApp);
+const HikerApp = () => (
+  <App />
+);
+AppRegistry.registerComponent('hikerapp', () => HikerApp)
