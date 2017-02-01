b4d7b69feeb2d52e39b8e14f5b978f52262604d5
w4ndry <eriwandri@gmail.com>
2017-02-01 17:42:20 +0700

    Complete style for LoginScreen.js

--- app/components/Auth/LoginScreen.js
+++ app/components/Auth/LoginScreen.js
@@ -9,33 +9,20 @@ const logo = require('./../../images/logo.png');
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    padding: 20,
     flex: 1,
+    padding: 20,
     backgroundColor: '#dddddd',
   },
-  title: {
-    fontSize: 30,
-    alignSelf: 'center',
-    marginBottom: 30,
-  },
-  buttonText: {
-    fontSize: 18,
-    fontWeight: '400',
-    color: 'white',
-    alignSelf: 'center',
-  },
-  button: {
-    height: 36,
-    backgroundColor: '#2196F3',
-    borderColor: '#2196F3',
-    borderWidth: 1,
-    borderRadius: 2,
-    marginTop: 10,
+  logo: {
+    width: 120,
+    height: 120,
     marginBottom: 5,
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    elevation: 2,
+  },
+  otherlog: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 5,
+    marginBottom: 5,
   },
   facebook: {
     height: 48,
@@ -58,15 +45,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderRadius: 48,
   },
-  otherlog: {
-    marginTop: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '400',
+    color: 'white',
+    alignSelf: 'center',
   },
-  logo: {
-    width: 120,
-    height: 120,
-    resizeMode: 'cover',
+  button: {
+    height: 36,
+    backgroundColor: '#2196F3',
+    borderColor: '#2196F3',
+    borderWidth: 1,
+    borderRadius: 2,
+    marginTop: 5,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    elevation: 2,
   },
 });
 
@@ -76,7 +70,7 @@ const LoginScreen = ({ submitLogin, register, forgotPassword, updateUsername, up
       <View style={{ justifyContent: 'center', alignItems: 'center' }}>
         <Image source={logo} style={styles.logo} />
       </View>
-      <View style={{ alignItems: 'center', top: 10, marginBottom: 10 }} ><Text style={{ fontSize: 16, color: 'rgba(0,0,0,0.8)' }}>Login With</Text></View>
+      <View style={{ alignItems: 'center' }} ><Text style={{ fontSize: 14, color: '#2196F3' }}>Login With</Text></View>
       <View style={styles.otherlog}>
         <TouchableHighlight style={styles.facebook} onPress={() => FacebookLogin.getFacebookLogin()} underlayColor={'#99d9f4'}>
           <Image source={facebook} style={styles.facebook} />
@@ -88,13 +82,13 @@ const LoginScreen = ({ submitLogin, register, forgotPassword, updateUsername, up
           <Image source={twitter} style={styles.twitter} />
         </TouchableHighlight>
       </View>
-      <View style={{ alignItems: 'center', top: 10, marginBottom: 10, justifyContent: 'space-between', flexDirection: 'row' }}>
+      <View style={{ alignItems: 'center', justifyContent: 'center', flexDirection: 'row', marginTop: 10 }}>
         <View style={{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.5)', width: 140, height: 1, marginLeft: 5 }} />
-        <Text style={{ width: 20, margin: 5, color: 'rgba(0,0,0,0.8)' }}> Or </Text>
+        <Text style={{ width: 20, color: 'rgba(0,0,0,0.8)' }}> Or </Text>
         <View style={{ borderWidth: 1, borderColor: 'rgba(0,0,0,0.5)', width: 140, height: 1, marginRight: 5 }} />
       </View>
-      <TextInput style={{ paddingBottom: 8 }} onChangeText={(username) => updateUsername(username)} placeholder={"Username"} placeholderTextColor={'rgba(0,0,0,0.5)'}/>
-      <TextInput secureTextEntry={true} style={{paddingBottom: 8 }} onChangeText={(password) => updatePassword (password)} placeholder="Password" placeholderTextColor={'rgba(0,0,0,0.5)'}/>
+      <TextInput style={{ paddingBottom: 2 }} onChangeText={(username) => updateUsername(username)} placeholder={"Username"} placeholderTextColor={'rgba(0,0,0,0.5)'}/>
+      <TextInput secureTextEntry={true} style={{paddingBottom: 2 }} onChangeText={(password) => updatePassword (password)} placeholder="Password" placeholderTextColor={'rgba(0,0,0,0.5)'}/>
       <TouchableHighlight style={styles.button} onPress={() => submitLogin()} underlayColor={'#1E88E5'}>
         <Text style={styles.buttonText}>LOGIN</Text>
       </TouchableHighlight>
