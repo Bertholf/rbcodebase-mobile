d4b1e69922a2126e0389f9185ecc627fb3648408
Sutani Ghifari <sutanighifari@gmail.com>
2017-02-01 19:01:04 +0700

    update mokcery friendly

--- app/factories/user.js
+++ app/factories/user.js
@@ -1,6 +1,7 @@
 import faker from 'faker';
 
 const user = () => ({
+  id : faker.random.number(),
   first_name: faker.name.firstName(),
   last_name: faker.name.lastName(),
   follow: faker.name.follow(),
