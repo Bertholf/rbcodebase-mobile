978ad86187821223f908b6434dfce6e2329ca834
ahmadahmadi14 <ahmadahmadi140989@gmail.com>
2017-02-02 14:58:37 +0700

    revision looping notif for notification

--- app/factories/notif.js
+++ app/factories/notif.js
@@ -7,13 +7,5 @@ const notif = (total = 5) => {
       users.push(user());
     }
   return users;
-
-   //id: faker.random.number(),
-  // read: faker.random.boolean(),
-  // image: faker.image.avatar(),
-  // from: faker.name.firstName(),
-  //  message: faker.lorem.text(),
-  //  date_time : faker.date.weekday(),
-  // notification_total: faker.lorem.sentences(),
 };
 export default notif;
