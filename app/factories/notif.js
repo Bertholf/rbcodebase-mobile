fbadfdc21aca57ccf039ea495e1f226168a7951d
ahmadahmadi14 <ahmadahmadi140989@gmail.com>
2017-02-02 08:13:35 +0700

    update notif

--- app/factories/notif.js
+++ app/factories/notif.js
@@ -3,12 +3,12 @@ import user from './user';
 
 const notif = () => ({
   id: faker.random.number(),
-  user: faker.name.firstName(),
+  user: user().first_name.concat(' ', user().last_name),
   read: faker.random.boolean(),
   image: faker.image.avatar(),
   from: faker.name.firstName(),
   message: faker.lorem.text(),
-  date_time: faker.date.recent(),
+  date_time: faker.date.past(),
   notification_total: faker.lorem.sentences(),
 });
 export default notif;
