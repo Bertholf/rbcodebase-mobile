a2e241320cb61863f3b3462479cc93433ee7f30f
Prayudi Tirta Bayu <prayuditb@gmail.com>
2017-02-02 18:31:41 +0700

    add seveButton function

--- app/services/setting.js
+++ app/services/setting.js
@@ -2,5 +2,5 @@ import api from './api';
 
 export default {
   getSetting: () => api.get('/setting'),
-  setSetting: json => api.put('/setting', json),
+  updateSetting: json => api.put('/setting', { json }),
 };
