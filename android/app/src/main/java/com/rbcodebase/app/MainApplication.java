008225a057b00bf6a00811980680261c7d16df69
Mochamad Gufron <mgufronefendi@gmail.com>
2017-02-02 16:59:39 +0700

    add more content

--- android/app/src/main/java/com/rbcodebase/app/MainApplication.java
+++ android/app/src/main/java/com/rbcodebase/app/MainApplication.java
@@ -52,7 +52,7 @@ public class MainApplication extends Application implements ReactApplication {
               new ReactNativeMapboxGLPackage(),
               new ReactNativeLocalizationPackage(),
               new FBSDKPackage(mCallbackManager),
-              new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
+              new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)
       );
     }
   };
