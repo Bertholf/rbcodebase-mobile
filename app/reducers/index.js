4901af34e44c3ef03883205897eb62da5951c068
Mochamad Gufron <mgufronefendi@gmail.com>
2017-02-07 17:40:37 +0700

    integrate to redux

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import splashState from './splash';
 import auth from './auth';
+import tos from './tos';
 
 const currentState = (state = {}, action) => {
   return {};
 };
-const reducer = combineReducers({ currentState, splashState, auth });
+const reducer = combineReducers({ currentState, splashState, auth, tos });
 export default reducer;
