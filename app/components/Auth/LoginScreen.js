1d1eb65f6c2800ded3f3111850937d429fcc3e86
Prayudi Tirta Bayu <prayuditb@gmail.com>
2017-02-08 18:26:44 +0700

    #191 delete unnecessary comment

--- app/components/Auth/LoginScreen.js
+++ app/components/Auth/LoginScreen.js
@@ -21,10 +21,6 @@ const facebook = require('./../../images/login/facebook.png');
 const twitter = require('./../../images/login/twitter.png');
 const logo = require('./../../images/logo.png');
 
-// const LoginScreen = ({
-//   username, password, submitLogin, register,
-//   forgotPassword, updateUsername, updatePassword,
-//   loginWithGoogle, loginWithFacebook }) =>
 export default class LoginScreen extends Component {
   constructor(props) {
     super(props);
@@ -55,96 +51,84 @@ export default class LoginScreen extends Component {
   }
   render() {
     return (
-        <View style={styles.container}>
-          <ScrollView>
-            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-              <Image source={logo} style={styles.logo} />
-            </View>
-            <View style={{ alignItems: 'center' }} ><Text style={{ fontSize: 14, color: '#2196F3' }}>Login With</Text></View>
-            <View style={styles.otherlog}>
-              <TouchableHighlight style={styles.facebook} onPress={() => FacebookLogin.getFacebookLogin()} underlayColor={'#99d9f4'}>
-                <Image source={facebook} style={styles.facebook} />
-              </TouchableHighlight>
-              {/* <TouchableHighlight style={styles.google}
-              onPress={() => GoogleSignIn.getGoogleSignIn()}  underlayColor={'#99d9f4'}>
-              <Image source={google} style={styles.google} />
-            </TouchableHighlight> */}
-              <TouchableHighlight style={styles.twitter} onPress={() => Actions.timelineList()} underlayColor={'#99d9f4'}>
-                <Image source={twitter} style={styles.twitter} />
-              </TouchableHighlight>
-            </View>
-            <View style={{ alignItems: 'center', justifyContent: 'center', flexDirection: 'row', marginTop: 10 }}>
-              <View style={{ borderWidth: 0.5, borderColor: 'rgba(0,0,0,0.5)', width: 145, height: 1, marginLeft: 5 }} />
-              <Text style={{ width: 20, color: 'rgba(0,0,0,0.8)' }}> Or </Text>
-              <View style={{ borderWidth: 0.5, borderColor: 'rgba(0,0,0,0.5)', width: 145, height: 1, marginRight: 5 }} />
-            </View>
-            <TextInput
-              style={{ height: 40 }}
-              onChangeText={username => this.setState({ username, validUsername: true, isFail: false })}
-              placeholder={'Username'}
-              required
-            />
-            {this.state.validUsername ? <Text /> : (
-              <Text style={styles.wrong}>Usename cannot blank</Text>
-            )}
-            <TextInput
-              secureTextEntry style={{ height: 40 }}
-              onChangeText={password =>
-                this.setState({ password, validPassword: true, isFail: false })
-              }
-              placeholder="Password"
-            />
-            {this.state.validPassword ? <Text /> : (
-              <Text style={styles.wrong}>Password cannot blank</Text>
-            )}
-            {!this.state.loading ? (
-              <TouchableOpacity
-                activeOpacity={0.7}
-                style={styles.button}
-                onPress={() => this.validate()}
-              >
-                <Text style={styles.buttonText}>Login</Text>
-              </TouchableOpacity>
-            ) : (
-              <TouchableOpacity
-                activeOpacity={0.7}
-                style={styles.button}
-              >
-                <ActivityIndicator size={'large'} />
-              </TouchableOpacity>
-            )}
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <TouchableOpacity onPress={() => Actions.register()}>
-                <Text style={{ color: '#2196F3', margin: 10, textAlign: 'right' }}>
-                    Register
+      <View style={styles.container}>
+        <ScrollView>
+          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Image source={logo} style={styles.logo} />
+          </View>
+          <View style={{ alignItems: 'center' }} ><Text style={{ fontSize: 14, color: '#2196F3' }}>Login With</Text></View>
+          <View style={styles.otherlog}>
+            <TouchableHighlight style={styles.facebook} onPress={() => FacebookLogin.getFacebookLogin()} underlayColor={'#99d9f4'}>
+              <Image source={facebook} style={styles.facebook} />
+            </TouchableHighlight>
+            {/* <TouchableHighlight style={styles.google}
+            onPress={() => GoogleSignIn.getGoogleSignIn()}  underlayColor={'#99d9f4'}>
+            <Image source={google} style={styles.google} />
+          </TouchableHighlight> */}
+            <TouchableHighlight style={styles.twitter} onPress={() => Actions.timelineList()} underlayColor={'#99d9f4'}>
+              <Image source={twitter} style={styles.twitter} />
+            </TouchableHighlight>
+          </View>
+          <View style={{ alignItems: 'center', justifyContent: 'center', flexDirection: 'row', marginTop: 10 }}>
+            <View style={{ borderWidth: 0.5, borderColor: 'rgba(0,0,0,0.5)', width: 145, height: 1, marginLeft: 5 }} />
+            <Text style={{ width: 20, color: 'rgba(0,0,0,0.8)' }}> Or </Text>
+            <View style={{ borderWidth: 0.5, borderColor: 'rgba(0,0,0,0.5)', width: 145, height: 1, marginRight: 5 }} />
+          </View>
+          <TextInput
+            style={{ height: 40 }}
+            onChangeText={username =>
+              this.setState({ username, validUsername: true, isFail: false })}
+            placeholder={'Username'}
+            required
+          />
+          {this.state.validUsername ? <Text /> : (
+            <Text style={styles.wrong}>Usename cannot blank</Text>
+          )}
+          <TextInput
+            secureTextEntry style={{ height: 40 }}
+            onChangeText={password =>
+              this.setState({ password, validPassword: true, isFail: false })
+            }
+            placeholder="Password"
+          />
+          {this.state.validPassword ? <Text /> : (
+            <Text style={styles.wrong}>Password cannot blank</Text>
+          )}
+          {!this.state.loading ? (
+            <TouchableOpacity
+              activeOpacity={0.7}
+              style={styles.button}
+              onPress={() => this.validate()}
+            >
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity
+              activeOpacity={0.7}
+              style={styles.button}
+            >
+              <ActivityIndicator size={'large'} />
+            </TouchableOpacity>
+          )}
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <TouchableOpacity onPress={() => Actions.register()}>
+              <Text style={{ color: '#2196F3', margin: 10, textAlign: 'right' }}>
+                  Register
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Actions.forgotPassword()}>
+              <Text style={{ color: '#2196F3', margin: 10, textAlign: 'right' }}>
+                  Forgot Password
                 </Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => Actions.forgotPassword()}>
-                <Text style={{ color: '#2196F3', margin: 10, textAlign: 'right' }}>
-                    Forgot Password
-                  </Text>
-              </TouchableOpacity>
+            </TouchableOpacity>
+          </View>
+          {!this.state.isFail ? <Text /> : (
+            <View style={styles.errBox}>
+              <Text style={{ color: '#fff' }} >Username or Password not match</Text>
             </View>
-            {!this.state.isFail ? <Text /> : (
-              <View style={styles.errBox}>
-                <Text style={{ color: '#fff' }} >Username or Password not match</Text>
-              </View>
-            )}
+          )}
         </ScrollView>
-        </View>
+      </View>
     );
   }
 }
-
-// LoginScreen.propTypes = {
-//   // submitLogin: React.PropTypes.func.isRequired,
-//   // forgotPassword: React.PropTypes.func.isRequired,
-//   // loginWithGoogle: React.PropTypes.func.isRequired,
-//   // register: React.PropTypes.func.isRequired,
-//   // loginWithFacebook: React.PropTypes.func.isRequired,
-//   // loginWithTwitter: React.PropTypes.func.isRequired,
-//   // updatePassword: React.PropTypes.func.isRequired,
-//   // updateUsername: React.PropTypes.func.isRequired,
-// };
-//
-// module.exports = LoginScreen;
