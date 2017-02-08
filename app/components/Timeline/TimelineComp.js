fbfaff3e0dd2e2804114b8bedbd6e9dcc0667e9e
w4ndry <eriwandri@gmail.com>
2017-02-08 10:17:34 +0700

    Add state for like and unlike

--- app/components/Timeline/TimelineComp.js
+++ app/components/Timeline/TimelineComp.js
@@ -9,13 +9,17 @@ import {
   ListView
 } from 'react-native';
 import TimelineComment from './timelineComment';
+import TimelineRightNav from './TimelineRightNav';
+import Accordion from 'react-native-accordion';
+
+const imgLike = require('./../../images/ic_thumb_up_black_18dp.png');
+const imgUnLike = require('./../../images/ic_thumb_down_black_18dp.png');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   timelineContainer: {
-    // flex: 1,
     justifyContent: 'flex-start',
     paddingTop: 8,
     paddingLeft: 16,
@@ -82,154 +86,133 @@ const styles = StyleSheet.create({
     marginLeft: 20,
   },
 
-  card: {
-    flexDirection:'row',
-    justifyContent:'flex-start',
-    backgroundColor: '#2196F3',
-
-  },
-
-  text: {
-    fontSize: 10,
-    textAlign: 'right',
-    paddingTop:6,
-    color:'#f8f8ff',
-    backgroundColor: '#2196F3',
-    padding:8,
-    marginTop:2
-
-  },
-  mainView:{
-
-    backgroundColor:'#ffffff',
-
-  }
 });
+
+
 export default class MapMain extends Component {
   constructor(props) {
     super(props);
     const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
+      onPress: true,
       dataSource: ds.cloneWithRows([
         'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin',
       ]),
     };
   }
-  // renderRow(rowData) {
-  //   return (
-  //
-  //     <TouchableOpacity style={styles.mainView} onPress={() => props.moveToDetail()}>
-  //       <View style={styles.card}>
-  //         <Image source={{ uri: 'http://s3.amazonaws.com/37assets/svn/765-default-avatar.png' }} style={{ width: 75, height: 75, borderRadius: 70, margin: 6 }} />
-  //         <Text style={styles.nameProfile}>{rowData}</Text>
-  //       </View>
-  //       <Image source={{ uri: 'http://ke5ter.com/img/route.png' }} style={{ width: 325, height: 183, justifyContent: 'center', marginLeft: 9 }} />
-  //       <Text style={styles.text}>Tangkuban Perahu</Text>
-  //       <Text style={styles.text}>KM</Text>
-  //       <TimelineComment></TimelineComment>
-  //     </TouchableOpacity>
-  //   );
-  // }
-  render() {
+
+  onChangeImg() {
+    this.setState({
+      onPress: !this.state.onPress,
+    });
+  }
+
+  renderRow(rowData) {
     return (
-      <View style={styles.container}>
-        <View style={styles.timelineContainer}>
-          <View style={styles.about}>
-            <TouchableOpacity activeOpacity={0.7}>
-              <Image
-                source={{ uri: 'http://s3.amazonaws.com/37assets/svn/765-default-avatar.png' }}
-                style={styles.avatarImg}
-              />
-            </TouchableOpacity>
-            <View style={styles.textAboutContainer}>
-              <Text style={styles.textNameProfile}>Mulia Arifandi Nasution</Text>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Image
-                  source={require('./../../images/ic_watch_later_black_18dp.png')}
-                  style={{ marginRight: 5, height: 10, width: 10 }}
-                />
-                <Text style={styles.textDay}>3 days ago</Text>
-              </View>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Image
-                  source={require('./../../images/ic_landscape_black_18dp.png')}
-                  style={{ marginRight: 3, height: 13, width: 13 }}
-                />
-                <Text style={styles.textDay}>Mount salak</Text>
-              </View>
-            </View>
-            <TouchableOpacity activeOpacity={0.7} style={styles.iconRightContainer}>
-              <Image
-                source={require('./../../images/ic_more_vert_black_24dp.png')}
-                style={styles.iconRightMenu}
-              />
-            </TouchableOpacity>
+  <View style={styles.container}>
+    <View style={styles.timelineContainer}>
+      <View style={styles.about}>
+        <TouchableOpacity activeOpacity={0.7}>
+          <Image
+            source={{ uri: 'http://s3.amazonaws.com/37assets/svn/765-default-avatar.png' }}
+            style={styles.avatarImg}
+          />
+        </TouchableOpacity>
+        <View style={styles.textAboutContainer}>
+          <Text style={styles.textNameProfile}>{rowData}</Text>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Image
+              source={require('./../../images/ic_watch_later_black_18dp.png')}
+              style={{ marginRight: 5, height: 10, width: 10 }}
+            />
+            <Text style={styles.textDay}>3 days ago</Text>
           </View>
-          <View style={styles.statusContainer}>
-            <Text style={styles.textStatus}>
-              Akhirnya sampai juga, Waktunya berlari meraih dann mimpi . . .
-            </Text>
-          </View>
-          <View style={styles.mapContainer}>
-            <Image source={{ uri: 'http://ke5ter.com/img/route.png' }} style={{ height: 183, justifyContent: 'center'}} />
-            <View style={styles.commentsCountContainer}>
-              <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={require('./../../images/ic_thumb_up_black_18dp.png')}
-                  style={{ marginRight: 5, height: 14, width: 14 }}
-                />
-                <Text style={styles.textLike}>100 Likes</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={require('./../../images/insert_comment_black.png')}
-                  style={{ marginRight: 5, height: 14, width: 14 }}
-                />
-              <Text>200 Comments</Text>
-            </TouchableOpacity>
-            </View>
-            <View style={styles.commentContainer}>
-              <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={require('./../../images/ic_thumb_up_black_18dp.png')}
-                  style={{ marginRight: 10, height: 15, width: 15 }}
-                />
-                <Text>Likes</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={require('./../../images/insert_comment_black.png')}
-                  style={{ marginRight: 10, height: 15, width: 15 }}
-                />
-                <Text>Comment</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={require('./../../images/share_black.png')}
-                  style={{ marginRight: 10, height: 15, width: 15 }}
-                />
-                <Text>Share</Text>
-              </TouchableOpacity>
-            </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Image
+              source={require('./../../images/ic_landscape_black_18dp.png')}
+              style={{ marginRight: 3, height: 13, width: 13 }}
+            />
+            <Text style={styles.textDay}>Mount salak</Text>
           </View>
         </View>
-        <View style={{ height: 10, backgroundColor: '#aaa' }}></View>
+        <TimelineRightNav />
       </View>
+      <View style={styles.statusContainer}>
+        <Text style={styles.textStatus}>
+          Akhirnya sampai juga, Waktunya berlari meraih dann mimpi . . .
+        </Text>
+      </View>
+      <View style={styles.mapContainer}>
+        <Image source={{ uri: 'http://ke5ter.com/img/route.png' }} style={{ height: 183, justifyContent: 'center'}} />
+        <View style={styles.commentsCountContainer}>
+          <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}
+            activeOpacity={0.7}
+          >
+            <Image
+              source={require('./../../images/ic_thumb_up_black_18dp.png')}
+              style={{ marginRight: 5, height: 14, width: 14 }}
+            />
+            <Text style={styles.textLike}>100 Likes</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }}
+            activeOpacity={0.7}
+          >
+            <Image
+              source={require('./../../images/insert_comment_black.png')}
+              style={{ marginRight: 5, height: 14, width: 14 }}
+            />
+          <Text>200 Comments</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={styles.commentContainer}>
+          <TouchableOpacity
+            onPress={() => this.onChangeImg()}
+            style={{ flexDirection: 'row', alignItems: 'center' }}
+            activeOpacity={0.7}
+          >
+            <Image
+              source={this.state.onPress ? imgLike : imgUnLike}
+              style={{ marginRight: 10, height: 15, width: 15 }}
+            />
+            <Text>{this.state.onPress ? 'Like' : 'Unlike'}</Text>
+          </TouchableOpacity>
 
-      // <ListView
-      //   dataSource={this.state.dataSource}
-      //   renderRow={rowData => this.renderRow(rowData)}
-      // />
+
+            <TouchableOpacity
+              style={{ flexDirection: 'row', alignItems: 'center' }}
+              activeOpacity={0.7}
+            >
+              <Image
+                source={require('./../../images/insert_comment_black.png')}
+                style={{ marginRight: 10, height: 15, width: 15 }}
+              />
+              <Text>Comment</Text>
+            </TouchableOpacity>
+
+
+          <TouchableOpacity
+            style={{ flexDirection: 'row', alignItems: 'center' }}
+            activeOpacity={0.7}
+          >
+            <Image
+              source={require('./../../images/share_black.png')}
+              style={{ marginRight: 10, height: 15, width: 15 }}
+            />
+            <Text>Share</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+    <View style={{ height: 10, backgroundColor: '#aaa' }}></View>
+  </View>
+     );
+   }
+  render() {
+    return (
+        <ListView
+         dataSource={this.state.dataSource}
+         renderRow={rowData => this.renderRow(rowData)}
+       />
     );
   }
 }
