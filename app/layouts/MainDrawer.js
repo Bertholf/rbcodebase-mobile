4d006b19bf8dfe999875303fb649e6ee14de8815
Prayudi Tirta Bayu <prayuditb@gmail.com>
2017-02-09 23:07:49 +0700

    #219 modify services/logout.js

--- app/layouts/MainDrawer.js
+++ app/layouts/MainDrawer.js
@@ -11,7 +11,7 @@ import {
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Actions } from 'react-native-router-flux';
 import me from '../services/me';
-
+import Logout from '../services/logout';
 
 const { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create({
@@ -81,8 +81,10 @@ export default class MainDrawer extends Component {
     this.setState({
       name: item.name,
     });
-
-    if (item.action) {
+    if (item.action === 'logout') {
+      this.props.navigate();
+      Logout();
+    } else if (item.action) {
       Actions[item.action]({ ...item.props });
       this.props.navigate();
     }
--- app/services/logout.js
+++ app/services/logout.js
@@ -1,17 +1,22 @@
-import { Acctions } from 'react-native-router-flux';
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage, NativeModules, Platform } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { LoginManager } from 'react-native-fbsdk';
 
-
+const google = NativeModules.GoogleSignInModule;
 const Logout = () => {
-  try{
-    const accessToken = AsyncStorage.getItem();
-    if (accessToken !== null && accessToken !== '') {
-      AsyncStorage.removeItem('accessToken', () => {
-        console.log('Failed to delete token');
-      });
-    }
-  } catch(err) {
-
+  const accessToken = AsyncStorage.getItem('accessToken');
+  const provider = AsyncStorage.getItem('provider');
+  if (provider === 'facebook' && accessToken !== null && typeof accessToken !== undefined) {
+    AsyncStorage.clear();
+    LoginManager.logOut();
+    Actions.login();
+  } else if (provider === 'google' && accessToken !== null && typeof accessToken !== undefined){
+    AsyncStorage.clear();
+    google.signOut();
+    Actions.login();
+  } else {
+    AsyncStorage.clear();
+    Actions.login();
   }
 }
 export default Logout;
