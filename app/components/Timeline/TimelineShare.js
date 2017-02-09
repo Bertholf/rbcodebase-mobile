f37098fd667b138e8ceabbad6de0c8c544739084
w4ndry <eriwandri@gmail.com>
2017-02-09 15:58:59 +0700

    #238 Fixed created new screen for file TimelineShare.js

--- app/components/Timeline/TimelineShare.js
+++ app/components/Timeline/TimelineShare.js
@@ -44,28 +44,43 @@ export default class TimelineShare extends Component {
   render() {
     if (this.state.loading === false) {
       return (
-        <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, backgroundColor: '#aaa' }}>
+          <View style={{ flex: 1, justifyContent: 'center' }}>
+            <View style={{ backgroundColor: 'white' }}>
           <TextInput
             style={{ height: 50, paddingLeft: 30 }}
             placeholder="Send To"
             onChangeText={(text) => this.setState({ text })}
           />
+          <View>
           <ListView
+            horizontal={true}
             dataSource={ds.cloneWithRows(this.state.friendlist.data)} renderRow={(rowData) =>
-              <TouchableOpacity style={{ flexDirection: 'row', borderBottomWidth:1, borderColor: '#aaa', paddingLeft: 10 }}>
-                <View>
+              <TouchableOpacity
+                activeOpacity={0.7}
+                style={{ borderBottomWidth:1, borderColor: '#aaa', paddingLeft: 10 }}>
+                <View style={{ alignItems: 'center', marginLeft: 10 }}>
                   <Image source={{ uri: rowData.image }} style={styles.photo} />
-                </View>
-                <View style={{ justifyContent: 'center', marginLeft: 10 }}>
                   <Text style={{ fontSize: 15 }}>{rowData.first_name}</Text>
                   <Text style={{ fontSize: 13, fontStyle: 'italic' }}>{rowData.username}</Text>
                 </View>
               </TouchableOpacity>
             }
           />
-          <TouchableOpacity style={{ padding: 10, backgroundColor: '#2196F3' }}>
-            <Text style={{ textAlign: 'center', color: 'white' }}> CANCEL </Text>
-          </TouchableOpacity>
+        </View>
+          <View style={{ padding: 5, borderWidth: 1, borderColor: '#aaa' }}>
+            <TextInput
+              multiline = {true}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+              placeholder="Write a message"
+            />
+            <TouchableOpacity activeOpacity={0.7} style={{ padding: 10, backgroundColor: '#2196F3', borderRadius: 3 }}>
+              <Text style={{ textAlign: 'center', color: 'white' }}> SEND </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        </View>
         </View>
       );
     } else {
