204a7420a4e17ac3204ba044cd518d41bb05f4cf
Rangga Adhitya <deneuv3.4@gmail.com>
2017-02-09 21:45:34 +0700

    add config for oauth

--- app/config.js
+++ app/config.js
@@ -1,4 +1,6 @@
 export default {
   FACEBOOK_APP_ID: '1748905725336743',
   FACEBOOK_APP_SECRET: '4d9e95344804626dc231fefd97fb1750',
+  client_id: '2',
+  client_secret: 'F3cAPA5doHFSt83rQ5TYsh3BMopvZb6B0tasb2nm',
 };
