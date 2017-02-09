399e57d19eab3257a05ad943ceb479d036722e15
putraranji <ogidoank51@yahoo.com>
2017-02-09 18:16:23 +0700

    Change like and dislike to sparated from other list

--- app/components/Timeline/TimelineComp.js
+++ app/components/Timeline/TimelineComp.js
@@ -14,6 +14,7 @@ import {
 import { Actions } from 'react-native-router-flux';
 import timelineList from '../../services/timelineList';
 import PostCard from './../Timeline/StatusPostCard/StatusCard';
+import TimelineList from './../Timeline/TimelineList';
 import TimelineComment from './timelineComment';
 import TimelineRightNav from './TimelineRightNav';
 import Accordion from 'react-native-accordion';
@@ -108,7 +109,7 @@ export default class MapMain extends Component {
     timelineList.getTimeline()
     .then((data) => {
       this.setState({ list: data, loading: false });
-    }).catch(({ err }) => console.error('SORY ERROR!!!!!!', err));
+    });
   }
 
   onChangeImg() {
@@ -116,110 +117,11 @@ export default class MapMain extends Component {
       onPress: !this.state.onPress,
     });
   }
-
   renderRow(dataPost) {
     return (
-      <View style={styles.container}>
-        <View style={styles.timelineContainer}>
-          <View style={styles.about}>
-            <TouchableOpacity activeOpacity={0.7} onPress={Actions.profile}>
-              <Image
-                source={{ uri: dataPost.avatarTimeline }}
-                style={styles.avatarImg}
-              />
-            </TouchableOpacity>
-            <View style={styles.textAboutContainer}>
-              <TouchableOpacity activeOpacity={0.7} onPress={Actions.profile}>
-                <Text style={styles.textNameProfile}>{dataPost.user}</Text>
-              </TouchableOpacity>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Image
-                  source={require('./../../images/ic_watch_later_black_18dp.png')}
-                  style={{ marginRight: 5, height: 10, width: 10 }}
-                />
-                <Text style={styles.textDay}>10 days ago</Text>
-              </View>
-              <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <Image
-                  source={require('./../../images/ic_landscape_black_18dp.png')}
-                  style={{ marginRight: 3, height: 13, width: 13 }}
-                />
-                <Text style={styles.textDay}>Mount salak</Text>
-              </View>
-            </View>
-            <TimelineRightNav />
-          </View>
-          <View style={styles.statusContainer}>
-            <Text style={styles.textStatus}>
-              Akhirnya sampai juga, Waktunya berlari meraih dann mimpi . . .
-            </Text>
-          </View>
-          <View style={styles.mapContainer}>
-            <Image source={{ uri: dataPost.imageTimeline }} style={{ height: 183, justifyContent: 'center' }} />
-            <View style={styles.commentsCountContainer}>
-              <TouchableOpacity
-                style={{ flexDirection: 'row', alignItems: 'center' }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={require('./../../images/ic_thumb_up_black_18dp.png')}
-                  style={{ marginRight: 5, height: 14, width: 14 }}
-                />
-                <Text style={styles.textLike}>{dataPost.numberTimeline} Likes</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{ flexDirection: 'row', alignItems: 'center' }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={require('./../../images/insert_comment_black.png')}
-                  style={{ marginRight: 5, height: 14, width: 14 }}
-                />
-                <Text>{dataPost.numberTimeline} Comments</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.commentContainer}>
-              <TouchableOpacity
-                onPress={() => this.onChangeImg()}
-                style={{ flexDirection: 'row', alignItems: 'center' }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={this.state.onPress ? imgLike : imgUnLike}
-                  style={{ marginRight: 10, height: 15, width: 15 }}
-                />
-                <Text>{this.state.onPress ? 'Like' : 'Unlike'}</Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={{ flexDirection: 'row', alignItems: 'center' }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={require('./../../images/insert_comment_black.png')}
-                  style={{ marginRight: 10, height: 15, width: 15 }}
-                />
-                <Text>Comment</Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={{ flexDirection: 'row', alignItems: 'center' }}
-                activeOpacity={0.7}
-              >
-                <Image
-                  source={require('./../../images/share_black.png')}
-                  style={{ marginRight: 10, height: 15, width: 15 }}
-                />
-                <Text>Share</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-        <View style={{ height: 10, backgroundColor: '#aaa' }} />
-      </View>
+      <TimelineList dataPost={dataPost} />
     );
   }
-
   render() {
     if (this.state.loading === false) {
       return (
