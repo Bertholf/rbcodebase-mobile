62783dc5c6e57dbd39654b7cc8f3bc2094371d82
Rangga Adhitya <deneuv3.4@gmail.com>
2017-02-10 13:16:33 +0700

    #255 add client_id & client_secret

--- app/config.js
+++ app/config.js
@@ -1,6 +1,7 @@
 export default {
   FACEBOOK_APP_ID: '1748905725336743',
   FACEBOOK_APP_SECRET: '4d9e95344804626dc231fefd97fb1750',
-  client_id: '2',
-  client_secret: 'F3cAPA5doHFSt83rQ5TYsh3BMopvZb6B0tasb2nm',
+  CLIENT_ID: '2',
+  CLIENT_SECRET: 'F3cAPA5doHFSt83rQ5TYsh3BMopvZb6B0tasb2nm',
+  GRANT_TYPE: 'password'
 };
