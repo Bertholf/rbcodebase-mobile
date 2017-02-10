f188be2fe9e9afb9a29377c91186a6d2edddc5a9
ahmadahmadi14 <ahmadahmadi140989@gmail.com>
2017-02-10 14:11:14 +0700

    225_create_location

--- app/components/Timeline/StatusPostCard/createLocation.js
+++ app/components/Timeline/StatusPostCard/createLocation.js
@@ -1,39 +1,60 @@
-import React, { Component } from 'react';
-import React, { DeviceEventEmitter, StyleSheet, Text,View, } from 'react-native';
-const RNALocation = require('react-native-android-location');
+import React, { Component} from 'react';
+//var React = require('react-native');
 
 
-const RNALocation = React.createClass({
-  // Create and Reset initial State Longitude (lng) and Latitude (lat)
-  getInitialState() { return { lng: 0.0, lat: 0.0}; },
+const {GooglePlacesAutocomplete} = require('react-native-google-places-autocomplete');
 
-  componentDidMount() {
-  	  // Register Listener Callback !Important!
-      DeviceEventEmitter.addListener('updateLocation',(e: Event) {
-          this.setState({lng: e.Longitude, lat: e.Latitude });
-      }.bind(this));
-      // Initialize RNALocation
-      RNALocation.getLocation();
-  },
+const homePlace = {description: 'Home', geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }};
+const workPlace = {description: 'Work', geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }};
 
+export default class Location  extends React.Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.location}>
-          Lng: {this.state.lng} Lat: {this.state.lat}
-        </Text>
-      </View>
-    );
-  }
-});
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        minLength={2}
+        autoFocus={false}
+        fetchDetails={true}
+        onPress={(data, details = null) => {
+          console.log(data);
+          console.log(details);
+        }}
+        getDefaultValue={() => {
+          return '';
+        }}
+        query={{
+          key: 'AIzaSyAF6YjswSgvBy9tmcHlIt32l9sRJ_6gOXY',
+          language: 'en', // language of the results
+          types: '(cities)', // default: 'geocode'
+        }}
+        styles={{
+          description: {
+            fontWeight: 'bold',
+          },
+          predefinedPlacesDescription: {
+            color: '#1faadb',
+          },
+        }}
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  location: {
-    textAlign: 'center'
-  }
-});
+        currentLocation={true}
+        currentLocationLabel="Current location"
+        nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+        GoogleReverseGeocodingQuery={{
+          // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
+        }}
+        GooglePlacesSearchQuery={{
+          // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+          rankby: 'distance',
+          types: 'food',
+        }}
+
+
+        filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+
+        predefinedPlaces={[homePlace, workPlace]}
+
+        predefinedPlacesAlwaysVisible={true}
+      />
+    );
+}
+}
