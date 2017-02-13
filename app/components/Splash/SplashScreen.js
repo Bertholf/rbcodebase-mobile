17426c726dd7ba53b53dd5d87bc7af0fc810e349
sutani <sutanighifari@gmail.com>
2017-02-13 19:49:57 +0700

    296-splashscreen - update splash with bcakground image

--- app/components/Splash/SplashScreen.js
+++ app/components/Splash/SplashScreen.js
@@ -3,18 +3,25 @@ import { View, Text, Image, ActivityIndicator } from 'react-native';
 
 
 const SplashScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#ffffff', paddingBottom: 20 }}>
+  <Image  source={require('./../../images/splash.jpg')} style={{ 	width: null,
+		height: null,
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'rgba(0,0,0,0)', }}>
     <Image
       source={require('./../../images/logo.png')}
       style={{
         width: 150,
         height: 150,
-        borderRadius: 75 }}
+        borderRadius: 75,
+
+       }}
     />
-    <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#1565c0', marginTop: 16 }}>
+    <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'white', marginTop: 16 }}>
       RBCodebase
     </Text>
-    <ActivityIndicator color="#2196f3" size="large" />
-  </View>
+    <ActivityIndicator color="white" size="large" />
+  </Image>
 );
 export default SplashScreen;
