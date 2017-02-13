cd0210c0a2d0b86a0e744c802a30347795863147
Rangga Adhitya <deneuv3.4@gmail.com>
2017-02-13 13:43:50 +0700

    #289-add text for register button

--- app/components/Auth/RegisterScreen.js
+++ app/components/Auth/RegisterScreen.js
@@ -100,181 +100,31 @@ export default class Register extends Component {
               <TouchableOpacity
                 activeOpacity={0.7} onPress={() => FacebookLogin.getFacebookLogin()}
               >
-                <Image source={facebook} style={styles.icon} />
+                <View>
+                  <Image source={facebook} style={styles.icon} />
+                  <Text>Register With Facebook</Text>
+                </View>
               </TouchableOpacity>
               <TouchableOpacity activeOpacity={0.7} onPress={() => this.register()} >
                 <Image source={google} style={styles.icon} />
+                <Text>Register With Google</Text>
               </TouchableOpacity>
               <TouchableOpacity activeOpacity={0.7} onPress={() => this.register()}>
                 <Image source={twitter} style={styles.icon} />
+                <Text>Register With Twitter</Text>
+              </TouchableOpacity>
+              <View style={{ alignItems: 'center', top: 10, marginBottom: 10, justifyContent: 'space-between', flexDirection: 'row' }}>
+                <View style={{ borderWidth: 1, borderColor: 'silver', width: 140, height: 1, marginLeft: 5 }} />
+                <Text style={{ width: 20, marginRight: 5, marginLeft: 5, top: -7, color: 'silver' }}> OR </Text>
+                <View style={{ borderWidth: 1, borderColor: 'silver', width: 140, height: 1, marginRight: 5 }} />
+                </View>
+              </View>
+              <TouchableOpacity activeOpacity={0.7} onPress={() => this.register()}>
+                <Image source={twitter} style={styles.icon} />
+                <Text>Register With Email</Text>
               </TouchableOpacity>
             </View>
           </View>
-          <View style={{ alignItems: 'center', top: 10, marginBottom: 10, justifyContent: 'space-between', flexDirection: 'row' }}>
-            <View style={{ borderWidth: 1, borderColor: 'silver', width: 140, height: 1, marginLeft: 5 }} />
-            <Text style={{ width: 20, marginRight: 5, marginLeft: 5, top: -7, color: 'silver' }}> OR </Text>
-            <View style={{ borderWidth: 1, borderColor: 'silver', width: 140, height: 1, marginRight: 5 }} />
-          </View>
-        <View
-          style={{
-            marginLeft: 5,
-            marginRight: 5,
-            borderColor: '#2196f3',
-            borderWidth: 0.5,
-            borderRadius: 4,
-            height: 50,
-          }}
-        >
-          <TextInput
-            multiline={false}
-            maxLength={32}
-            placeholder={'First Name'}
-            selectionColor={'silver'}
-            underlineColorAndroid={'rgba(0,0,0,0)'}
-            style={{ marginLeft: 10, marginRight: 10, height: 50 }}
-            onChangeText={firstName => this.setState({ firstName, validFName: true, errMsg: '' })}
-          />
-        </View>
-          {this.state.validFName ? (<Text />)
-            : (<Text style={styles.wrong}>First name cannot contain numbers and simbols</Text>)
-          }
-        <View
-          style={{
-            marginLeft: 5,
-            marginRight: 5,
-            borderColor: '#2196f3',
-            borderWidth: 0.5,
-            borderRadius: 4,
-            height: 50,
-          }}
-        >
-          <TextInput
-            multiline={false}
-            maxLength={32}
-            placeholder={'Last Name'}
-            selectionColor={'silver'}
-            underlineColorAndroid={'rgba(0,0,0,0)'}
-            style={{ marginLeft: 10, marginRight: 10, height: 50 }}
-            onChangeText={lastName => this.setState({ lastName, validLName: true, errMsg: '' })}
-          />
-        </View>
-          {this.state.validLName ? (<Text />)
-            : (<Text style={styles.wrong}>Last name cannot contain numbers and simbols</Text>)
-          }
-          <View
-            style={{
-              marginLeft: 5,
-              marginRight: 5,
-              borderColor: '#2196f3',
-              borderWidth: 0.5,
-              borderRadius: 4,
-              height: 50,
-            }}
-          >
-            <TextInput
-              multiline={false}
-              keyboardType={'email-address'}
-              placeholder={'Email'}
-              selectionColor={'silver'}
-              underlineColorAndroid={'rgba(0,0,0,0)'}
-              style={{ marginLeft: 10, marginRight: 10, height: 50 }}
-              onChangeText={email => this.setState({ email, validEmail: true, errMsg: '' })}
-            />
-          </View>
-          {this.state.validEmail ? (<Text />)
-            : (<Text style={styles.wrong}>Please input valid email</Text>)
-          }
-          <View
-            style={{
-              marginLeft: 5,
-              marginRight: 5,
-              borderColor: '#2196f3',
-              borderWidth: 0.5,
-              borderRadius: 4,
-              height: 50,
-            }}
-          >
-            <TextInput
-              multiline={false}
-              placeholder={'Username'}
-              maxLength={32}
-              selectionColor={'silver'}
-              underlineColorAndroid={'rgba(0,0,0,0)'}
-              style={{ marginLeft: 10, marginRight: 10, height: 50 }}
-              onChangeText={username => this.setState({ username, validUsername: true, errMsg: '' })}
-            />
-          </View>
-          {this.state.validUsername ? (<Text />)
-            : (<Text style={styles.wrong}>Username just contain letter and number</Text>)
-          }
-          <View
-            style={{
-              marginLeft: 5,
-              marginRight: 5,
-              borderColor: '#2196f3',
-              borderWidth: 0.5,
-              borderRadius: 4,
-              height: 50,
-            }}
-          >
-            <TextInput
-              multiline={false}
-              placeholder={'Password'}
-              maxLength={32}
-              selectionColor={'silver'}
-              underlineColorAndroid={'rgba(0,0,0,0)'}
-              style={{ marginLeft: 10, marginRight: 10, height: 50 }}
-              secureTextEntry
-              onChangeText={password => this.setState({ password, validPass: true })}
-            />
-            </View>
-          {this.state.validPass ? (<Text />)
-            : (<Text style={styles.wrong}>Password cannot be blank</Text>)
-          }
-          {this.state.errMsg === '' ? <Text /> : (
-            <View style={styles.errBox}>
-              <Text style={{ color: '#fff' }}>{this.state.errMsg}</Text>
-            </View>
-          )}
-          <View style={{ alignItems: 'flex-start', width: width * 0.87, height: 20 }} >
-            <Text style={{ color: 'white' }}>Gender</Text>
-          </View>
-          <View style={styles.genderRow} >
-            <TouchableOpacity
-              activeOpacity={0.7}
-              style={[styles.btnGender, this.state.male && styles.active]}
-              onPress={() => this.setState({ male: true, female: false })}
-            >
-              <Image source={imgmale} style={[styles.imgGender, { tintColor: '#1565c0' }]} />
-              <Text style={{ color: '#1565c0' }}>Male</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              style={[styles.btnGender, this.state.female && styles.active2]}
-              onPress={() => this.setState({ female: true, male: false })}
-            >
-              <Image source={imgfemale} style={[styles.imgGender, { tintColor: '#DF2668' }]} />
-              <Text style={{ color: '#DF2668' }}>Female</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={{height: 40}}>
-            <Text>By clicking Register, I agree with
-              <Text style={{ color: '#2196F3' }} onPress={() => Actions.tos()}> Terms of Service</Text>
-            </Text>
-          </View>
-          {this.state.loading ? (
-            <View style={styles.btnReg}>
-              <ActivityIndicator size={'large'}/>
-            </View>
-          ) : (
-            <TouchableOpacity
-              activeOpacity={0.7} style={styles.btnReg}
-              onPress={() => this.validate()}
-            >
-              <Text style={styles.textReg}>Register</Text>
-            </TouchableOpacity>
-          )}
-        </View>
       </ScrollView>
     );
   }
