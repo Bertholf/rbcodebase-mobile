395fa8d6c1eac80ddefa7ac991bda22b96e90ce5
Aristyo Hadikusuma <tyoaris21@gmail.com>
2017-02-13 18:10:53 +0700

    add button walkthrough

--- app/components/Splash/Walkthrough.js
+++ app/components/Splash/Walkthrough.js
@@ -4,18 +4,19 @@ import {
   View,
   Image,
   Dimensions,
+  TouchableOpacity,
+  Button,
 } from 'react-native';
 import {
   IndicatorViewPager, PagerDotIndicator
 }from 'rn-viewpager';
-var { height,  width} = Dimensions.get('window');
-const {height } = Dimensions.get('window');
+const { height } = Dimensions.get('window');
 export default class walkthrough extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state =
     {
-      gambar: './Img/ic_account_balance_black_24dp.png',
+      gambar: './../../images/account.png',
       subtitle: 'INTRODUCTION2',
       subtitle2: 'WEATHER AND TRAIL CONDITIONS',
     };
@@ -34,39 +35,44 @@ export default class walkthrough extends Component {
           //   subtitle: "INTRODUCTION",
           //   subtitle2: "WEATHER AND TRAIL CONDITIONS",
           // };
-        }
+  }
   render() {
-      return (
-        <View style={{flex:1}}>
+    return (
+      <View style={{ flex: 1, backgroundColor: 'grey' }}>
           <IndicatorViewPager
-            style={{ height: height * 0.98 }}
+            style={{ height: height * 0.91 }}
             indicator={this._renderDotIndicator()}
           >
             <View style={{ backgroundColor:'#f1c40f', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
               <Image source={require('./../../images/account.png')} />
-              <Text style= {{fontWeight: 'bold'}}>ENJOY UNLIMITED PAYES</Text>
+              <Text style={{ fontWeight: 'bold'}}>ENJOY UNLIMITED PAYES</Text>
               <Text>The 93 mile long Wonderland Trail circumnavigates Mt Rainier withinroad and can be done as day hikes</Text>
             </View>
             <View style={{backgroundColor:'#2ecc71',flexDirection:'column',justifyContent:'center',alignItems: 'center'}}>
               <Image source={require('./../../images/account.png')} />
               <Text style= {{fontWeight: 'bold'}}>ENJOY UNLIMITED PAYES</Text>
-              <Text>'The 93 mile long Wonderland Trail circumnavigates Mt Rainier withinroad and can be done as day hikes',</Text>
+              <Text>The 93 mile long Wonderland Trail circumnavigates Mt Rainier withinroad and can be done as day hikes</Text>
             </View>
             <View style={{backgroundColor:'#2980b9',flexDirection:'column',justifyContent:'center',alignItems: 'center'}}>
                 {/* <Image source={require('./Img/ic_perm_media_black_24dp.png')}></Image> */}
                 <Image source={require('./../../images/account.png')} />
                 <Text style= {{fontWeight: 'bold'}}>ENJOY UNLIMITED PAYES</Text>
-                <Text>'The 93 mile long Wonderland Trail circumnavigates Mt Rainier withinroad and can be done as day hikes',</Text>
+                <Text> The 93 mile long Wonderland Trail circumnavigates Mt Rainier withinroad and can be done as day hikes </Text>
             </View>
             <View style={{backgroundColor:'#e67e22',flexDirection:'column',justifyContent:'center',alignItems: 'center'}}>
               <Image source={require('./../../images/account.png')} />
               <Text style={{ fontWeight: 'bold'}} >ENJOY UNLIMITED PAYES</Text>
               <Text>The 93 mile long Wonderland Trail circumnavigates Mt Rainier withinroad and can be done as day hikes</Text>
             </View>
-        </IndicatorViewPager>
-
+          </IndicatorViewPager>
+        <View style={{ paddingTop: 1 }}>
+          <Button
+            title="Get Started"
+            color="#2196F3"
+          />
         </View>
-      );
+      </View>
+    );
   }
   _renderTitleIndicator() {
     return <PagerTitleIndicator titles={['one', 'two', 'three']} />;
