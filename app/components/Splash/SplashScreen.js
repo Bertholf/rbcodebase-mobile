a9ff61d7a6d63aec72c2b401c3f616b220215803
sutani <sutanighifari@gmail.com>
2017-02-13 17:01:48 +0700

    296-splashscreen -change background splash screen

--- app/components/Splash/SplashScreen.js
+++ app/components/Splash/SplashScreen.js
@@ -3,18 +3,20 @@ import { View, Text, Image, ActivityIndicator } from 'react-native';
 
 
 const SplashScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#ffffff', paddingBottom: 20 }}>
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#64DD17', paddingBottom: 20, opacity:0.5 }}>
     <Image
       source={require('./../../images/logo.png')}
       style={{
         width: 150,
         height: 150,
-        borderRadius: 75 }}
+        borderRadius: 75,
+
+       }}
     />
-    <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#1565c0', marginTop: 16 }}>
+    <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#2E7D32', marginTop: 16 }}>
       RBCodebase
     </Text>
-    <ActivityIndicator color="#2196f3" size="large" />
+    <ActivityIndicator color="white" size="large" />
   </View>
 );
 export default SplashScreen;
