ea382956a0300d65c98b278845cdc127c49debd1
w4ndry <eriwandri@gmail.com>
2017-02-14 14:26:19 +0700

    325 add button for swiper to dashboard

--- app/components/UserPanel/UserPanel.js
+++ app/components/UserPanel/UserPanel.js
@@ -5,35 +5,45 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native';
+import styles from './../../components/UserPanel/style';
 
 const settingIcon = require('./../../images/ic_settings_black_24dp.png');
 const userImage = require('./../../images/profile-pic.jpg');
+const followImage = require('./../../images/ic_attach_file_white_24dp.png');
+const contactImage = require('./../../images/facebook-square.png');
+const addFriendImage = require('./../../images/friend.png');
+
 
 const userPanel = () => (
   <View style={{ flex: 1, backgroundColor: '#fff' }}>
     <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-      <TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.7}>
         <Image source={settingIcon} style={{ margin: 15, width: 25, height: 25 }} />
       </TouchableOpacity>
     </View>
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',  }}>
-      <View style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
+    <View style={{ justifyContent: 'flex-start', alignItems: 'center' }} >
+      <TouchableOpacity activeOpacity={0.7} style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
         <Image source={userImage} style={{ width: 130, height: 130, borderRadius: 130 }} />
+      </TouchableOpacity>
+      <View style={{ flexDirection: 'column', marginTop: 20 }}>
+        <TouchableOpacity activeOpacity={0.7} style={styles.imgLinksContainer}>
+          <Image source={followImage} style={styles.imgLinks} />
+          <Text style={styles.textLinks}>Following Me </Text>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.7} style={styles.imgLinksContainer}>
+          <Image source={contactImage} style={styles.imgLinks} />
+          <Text style={styles.textLinks}>My Friends </Text>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.7} style={styles.imgLinksContainer}>
+          <Image source={addFriendImage} style={styles.imgLinks} />
+          <Text style={styles.textLinks}>Add Friends </Text>
+        </TouchableOpacity>
       </View>
-      <View style={{ flexDirection: 'column', backgroundColor: 'blue', marginTop: 20 }}>
-        <View>
-          <Image />
-          <Text>Following Me </Text>
-        </View>
-        <View>
-          <Image />
-          <Text>My Friends </Text>
-        </View>
-        <View>
-          <Image />
-          <Text>Add Friends </Text>
-        </View>
-      </View>
+    </View>
+    <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+      <TouchableOpacity activeOpacity={0.7}>
+        <Image source={settingIcon} style={{ margin: 15, width: 25, height: 25 }} />
+      </TouchableOpacity>
     </View>
 
   </View>
