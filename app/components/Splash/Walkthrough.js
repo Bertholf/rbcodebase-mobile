81341cae2c0a781ce19d676fa257c34d3f5df8b1
Rangga Adhitya <deneuv3.4@gmail.com>
2017-02-14 19:29:24 +0700

    fix walkthrough style

--- app/components/Splash/Walkthrough.js
+++ app/components/Splash/Walkthrough.js
@@ -53,18 +53,16 @@ export default class walkthrough extends Component {
             <View style={{flex: 4, flexDirection: 'column', alignItems: 'center', padding: 20, justifyContent: 'center' }}>
                 <Text style={{fontWeight: 'bold', fontSize: 20, marginBottom: 10}}>ENJOY UNLIMITED PAYES</Text>
                 <Text>The 93 mile long Wonderland Trail circumnavigates Mt Rainier withinroad and can be done as day hikes</Text>
-              </View>
+            </View>
             </View>
           </IndicatorViewPager>
         <View style={{ backgroundColor: '#2196F3', alignItems: 'center', justifyContent: 'center', height: 50 }}>
           <TouchableOpacity
             activeOpacity={0.7}
             onPress={Actions.login}
-            style={{ flex: 1, backgroundColor: '#2196F3', elevation: 2 }}
+            style={{ flex: 1, backgroundColor: '#2196F3'}}
           >
-            <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-              <Text style={{ padding: 10, fontWeight: '200', fontSize: 17, color: 'white', alignSelf: 'center', justifyContent: 'center' }}> GET STARTED </Text>
-            </View>
+            <Text style={{ padding: 10, fontWeight: '200', fontSize: 17, color: 'white', justifyContent: 'center' }}> GET STARTED </Text>
           </TouchableOpacity>
         </View>
       </View>
