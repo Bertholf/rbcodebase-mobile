4170f66df5f9ff29a2bcbfc2758ab0932cdfc653
w4ndry <eriwandri@gmail.com>
2017-02-14 14:18:18 +0700

    325 add new file name style.js

--- app/components/UserPanel/style.js
+++ app/components/UserPanel/style.js
@@ -1,7 +1,21 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.creat({
-
+const styles = StyleSheet.create({
+  imgLinksContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  imgLinks: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+  },
+  textLinks: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'rgba(0,0,0,0.9)',
+  },
 });
 
 export default styles;
