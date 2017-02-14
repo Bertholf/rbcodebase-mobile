ee7e5c7c1d816cf197fe9b47c8b760b873349c25
w4ndry <eriwandri@gmail.com>
2017-02-14 15:06:34 +0700

    325 move style to style.js

--- app/components/UserPanel/UserPanel.js
+++ app/components/UserPanel/UserPanel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  ScrollView,
   View,
   Image,
   Text,
@@ -9,43 +10,45 @@ import styles from './../../components/UserPanel/style';
 
 const settingIcon = require('./../../images/ic_settings_black_24dp.png');
 const userImage = require('./../../images/profile-pic.jpg');
-const followImage = require('./../../images/ic_attach_file_white_24dp.png');
-const contactImage = require('./../../images/facebook-square.png');
-const addFriendImage = require('./../../images/friend.png');
+const followIcon = require('./../../images/people.png');
+const contactIcon = require('./../../images/ic_contacts_black_24dp.png');
+const addFriendIcon = require('./../../images/ic_person_add_black_24dp.png');
+const bottomArrowIcon = require('./../../images/ic_expand_more_black_24dp.png');
 
 
 const userPanel = () => (
-  <View style={{ flex: 1, backgroundColor: '#fff' }}>
-    <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-      <TouchableOpacity activeOpacity={0.7}>
-        <Image source={settingIcon} style={{ margin: 15, width: 25, height: 25 }} />
-      </TouchableOpacity>
-    </View>
-    <View style={{ justifyContent: 'flex-start', alignItems: 'center' }} >
-      <TouchableOpacity activeOpacity={0.7} style={{ flexDirection: 'row', alignItems: 'flex-start' }}>
-        <Image source={userImage} style={{ width: 130, height: 130, borderRadius: 130 }} />
-      </TouchableOpacity>
-      <View style={{ flexDirection: 'column', marginTop: 20 }}>
-        <TouchableOpacity activeOpacity={0.7} style={styles.imgLinksContainer}>
-          <Image source={followImage} style={styles.imgLinks} />
-          <Text style={styles.textLinks}>Following Me </Text>
-        </TouchableOpacity>
-        <TouchableOpacity activeOpacity={0.7} style={styles.imgLinksContainer}>
-          <Image source={contactImage} style={styles.imgLinks} />
-          <Text style={styles.textLinks}>My Friends </Text>
-        </TouchableOpacity>
-        <TouchableOpacity activeOpacity={0.7} style={styles.imgLinksContainer}>
-          <Image source={addFriendImage} style={styles.imgLinks} />
-          <Text style={styles.textLinks}>Add Friends </Text>
+  <View style={styles.container}>
+    <ScrollView>
+      <View style={styles.btnSettingContainer}>
+        <TouchableOpacity activeOpacity={0.7}>
+          <Image source={settingIcon} style={styles.iconImage} />
         </TouchableOpacity>
       </View>
-    </View>
-    <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+      <View style={styles.linksContainer} >
+        <TouchableOpacity activeOpacity={0.7} style={styles.userButton}>
+          <Image source={userImage} style={styles.userImage} />
+        </TouchableOpacity>
+        <View style={{ flexDirection: 'column', marginTop: 20 }}>
+          <TouchableOpacity activeOpacity={0.7} style={styles.imgLinksContainer}>
+            <Image source={followIcon} style={styles.imgLinks} />
+            <Text style={styles.textLinks}>Following Me </Text>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.7} style={styles.imgLinksContainer}>
+            <Image source={contactIcon} style={styles.imgLinks} />
+            <Text style={styles.textLinks}>My Friends </Text>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.7} style={styles.imgLinksContainer}>
+            <Image source={addFriendIcon} style={styles.imgLinks} />
+            <Text style={styles.textLinks}>Add Friends </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
+    <View style={styles.swapContainer}>
       <TouchableOpacity activeOpacity={0.7}>
-        <Image source={settingIcon} style={{ margin: 15, width: 25, height: 25 }} />
+        <Image source={bottomArrowIcon} style={styles.swapImage} />
       </TouchableOpacity>
     </View>
-
   </View>
 );
 
--- app/components/UserPanel/style.js
+++ app/components/UserPanel/style.js
@@ -1,20 +1,55 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  btnSettingContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  iconImage: {
+    margin: 15,
+    width: 25,
+    height: 25,
+  },
+  linksContainer: {
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  userButton: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+  userImage: {
+    width: 130,
+    height: 130,
+    borderRadius: 130,
+  },
   imgLinksContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 30,
   },
   imgLinks: {
-    width: 30,
-    height: 30,
+    width: 40,
+    height: 40,
     marginRight: 10,
   },
   textLinks: {
-    fontSize: 15,
-    fontWeight: 'bold',
+    fontSize: 16,
+    fontWeight: '500',
     color: 'rgba(0,0,0,0.9)',
+  },
+  swapContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  swapImage: {
+    width: 70,
+    height: 50,
   },
 });
 
