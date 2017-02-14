2307b4c336e33a6a89db26f42ee9cf95e136ef89
Rangga Adhitya <deneuv3.4@gmail.com>
2017-02-14 23:15:32 +0700

    334 Fix wrong branch push

--- app/components/Setting/Setting.js
+++ app/components/Setting/Setting.js
@@ -3,50 +3,173 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import styles from './../../components/Setting/Style';
 
+const next = require('./../../images/ic_navigate_next_2x.png');
 
 const Setting = () => {
   return (
     <View style={{ flex: 1, backgroundColor: '#fff' }}>
       <ScrollView>
         <View style={styles.container}>
-          <Text>Account</Text>
+            <Text style={styles.titleText}>Account</Text>
           <TouchableOpacity onPress={Actions.account} >
             <View style={styles.list}>
-              <Text style={styles.text}>Name</Text>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Name</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{ alignSelf: 'center'}}>My Name</Text>
+                <Image style={styles.image} source={next} />
+              </View>
             </View>
           </TouchableOpacity>
           <TouchableOpacity onPress={Actions.privacy}>
             <View style={styles.list}>
-              <Text style={styles.text}>Username</Text>
+              <View style={{alignSelf: 'center' }}>
+                <Text style={styles.text}>Username</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{ alignSelf: 'center' }}>@tester</Text>
+                <Image style={styles.image} source={next} />
+              </View>
             </View>
           </TouchableOpacity>
           <TouchableOpacity onPress={Actions.email}>
             <View style={styles.list}>
-              <Text style={styles.text}>Birthday</Text>
+              <View style={{ alignSelf: 'center' }}>
+                <Text style={styles.text}>Birthday</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{ alignSelf: 'center'}}>1 Jan 2017</Text>
+                <Image style={styles.image} source={next} />
+              </View>
             </View>
           </TouchableOpacity>
           <TouchableOpacity onPress={Actions.account} >
             <View style={styles.list}>
-              <Text style={styles.text}>Mobile Number</Text>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Mobile Number</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{ alignSelf: 'center'}}>+621293823</Text>
+                <Image style={styles.image} source={next} />
+              </View>
             </View>
           </TouchableOpacity>
           <TouchableOpacity onPress={Actions.account} >
             <View style={styles.list}>
-              <Text style={styles.text}>Email</Text>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Email</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{ alignSelf: 'center'}}>email@domain.com</Text>
+                <Image style={styles.image} source={next} />
+              </View>
             </View>
           </TouchableOpacity>
           <TouchableOpacity onPress={Actions.account} >
             <View style={styles.list}>
-              <Text style={styles.text}>Password</Text>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Password</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{ alignSelf: 'center'}}>email@domain.com</Text>
+                <Image style={styles.image} source={next} />
+              </View>
             </View>
           </TouchableOpacity>
-          <Text>Preferences</Text>
-          <TouchableOpacity onPress={Actions.deactive}>
-            <View style={styles.list1}>
-              <Text style={styles.deactive}>Deactive</Text>
+          <Text style={styles.titleText}>Preference</Text>
+          <TouchableOpacity onPress={Actions.account} >
+            <View style={styles.list}>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Privacy</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Image style={styles.image} source={next} />
+              </View>
             </View>
           </TouchableOpacity>
-
+          <TouchableOpacity onPress={Actions.account} >
+            <View style={styles.list}>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Notificaitons</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Image style={styles.image} source={next} />
+              </View>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={Actions.account} >
+            <View style={styles.list}>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Ad Preference</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Image style={styles.image} source={next} />
+              </View>
+            </View>
+          </TouchableOpacity>
+          <Text style={styles.titleText}>More Information</Text>
+          <TouchableOpacity onPress={Actions.account} >
+            <View style={styles.list}>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Support</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Image style={styles.image} source={next} />
+              </View>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={Actions.account} >
+            <View style={styles.list}>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Privacy Policy</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Image style={styles.image} source={next} />
+              </View>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={Actions.account} >
+            <View style={styles.list}>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Term of Services</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Image style={styles.image} source={next} />
+              </View>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={Actions.account} >
+            <View style={styles.list}>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Licences</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Image style={styles.image} source={next} />
+              </View>
+            </View>
+          </TouchableOpacity>
+          <Text style={styles.titleText}>Account Actions</Text>
+          <TouchableOpacity onPress={Actions.account} >
+            <View style={styles.list}>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Cleaser Cache</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Image style={styles.image} source={next} />
+              </View>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={Actions.account} >
+            <View style={styles.list}>
+              <View style={{alignSelf: 'center'}}>
+                <Text style={styles.text}>Log Out</Text>
+              </View>
+              <View style={{flexDirection: 'row'}}>
+                <Image style={styles.image} source={next} />
+              </View>
+            </View>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </View>
--- app/components/Setting/Style.js
+++ app/components/Setting/Style.js
@@ -5,7 +5,7 @@ const { height } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
-    padding: 10,
+    padding: 20,
     elevation: 20,
   },
   imagesLeft: {
@@ -17,32 +17,41 @@ const styles = StyleSheet.create({
   list: {
     marginTop: 10,
     flexDirection: 'row',
-    padding: 15,
+    paddingLeft: 15,
     backgroundColor: '#ffffff',
     borderColor: '#2196F3',
     borderWidth: 0.8,
     borderRadius: 2,
-    justifyContent: 'flex-start',
+    justifyContent: 'space-between',
     marginBottom: 8,
+    height: 40,
   },
   list1: {
     backgroundColor: '#D50000',
-    padding: 5,
+    padding: 0,
     borderRadius: 3,
-    height: 50,
+    height: 35,
     elevation: 2,
     marginTop: 40,
     alignItems: 'center',
   },
   text: {
     color: '#000000',
-    fontSize: 20,
-    paddingLeft: 16,
+    fontSize: 13,
   },
   deactive: {
     fontSize: 25,
     color: 'white',
   },
+  titleText: {
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  image:{
+    height: 30,
+    width: 30,
+    alignSelf: 'center',
+  }
 });
 
 module.exports = styles;
