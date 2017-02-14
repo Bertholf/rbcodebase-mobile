941fcc3808ead4d56895348330958646e4d3ce5a
Mochamad Gufron <mgufronefendi@gmail.com>
2017-02-14 19:01:12 +0700

    update walkthrough style

--- app/components/Splash/Walkthrough.js
+++ app/components/Splash/Walkthrough.js
@@ -14,9 +14,9 @@ const { height, width } = Dimensions.get('window');
 export default class walkthrough extends Component {
   render() {
     return (
-      <View style={{ backgroundColor: '#2196F3' }}>
+      <View style={{ backgroundColor: '#2196F3', flex: 1 }}>
           <IndicatorViewPager
-            style={{ height: height * 0.88 }}
+            style={{ flex: 1 }}
             indicator={this._renderDotIndicator()}
           >
             <View style={{ flex: 1, backgroundColor:'#26A69A', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', paddingBottom: 50 }}>
@@ -56,14 +56,14 @@ export default class walkthrough extends Component {
               </View>
             </View>
           </IndicatorViewPager>
-        <View style={{ backgroundColor: '#2196F3', alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ backgroundColor: '#2196F3', alignItems: 'center', justifyContent: 'center', height: 50 }}>
           <TouchableOpacity
             activeOpacity={0.7}
             onPress={Actions.login}
-            style={{ height: 500, backgroundColor: '#2196F3', alignSelf: 'stretch', elevation: 2 }}
+            style={{ flex: 1, backgroundColor: '#2196F3', elevation: 2 }}
           >
-            <View style={{flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-              <Text style={{ padding: 15, fontWeight: '200', fontSize: 17, color: 'white', alignSelf: 'center', justifyContent: 'center' }}> GET STARTED </Text>
+            <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+              <Text style={{ padding: 10, fontWeight: '200', fontSize: 17, color: 'white', alignSelf: 'center', justifyContent: 'center' }}> GET STARTED </Text>
             </View>
           </TouchableOpacity>
         </View>
