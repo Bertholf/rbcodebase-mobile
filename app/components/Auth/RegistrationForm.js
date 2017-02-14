8438a5200671d20015ad5aa432c3270aa9efc632
shilohchis <shidei.chis@gmail.com>
2017-02-15 00:43:36 +0700

    #336: Fix invalid email validation

--- app/components/Auth/RegistrationForm.js
+++ app/components/Auth/RegistrationForm.js
@@ -97,9 +97,11 @@ export default class RegistrationForm extends Component {
     const emptyFName = this.state.firstname === '';
     const emptyLName = this.state.lastname === '';
     const emptyUName = this.state.username === '';
-    const emptyEmail = this.state.email === '';
+    const emptyEmail = this.state.email === '' || !this.state.email;
+    console.log(emptyEmail);
     const validUsername = this.state.username.length > 4;
     const validEmail = emailRegex.test(this.state.email);
+    // console.log(validEmail);
     const validPass = (this.state.password === this.state.confirmPassword);
     const validLPass = this.state.password.length >= 6;
     const emptyPass = this.state.password === '';
