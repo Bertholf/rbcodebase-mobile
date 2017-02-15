5533dc1367c15be058e07e31938215ffbd5c8508
ahmadahmadi14 <ahmadahmadi140989@gmail.com>
2017-02-15 20:36:51 +0700

    #359 move folder localizations to app/localizations from app/components/localizations

---
+++ app/localizations/id/register.js
@@ -0,0 +1,34 @@
+import LocalizedStrings from 'react-native-localization';
+
+
+const register = {
+  RegisterWith: 'Daftar dengan:',
+  Registerfacebook: 'Register dengan Facebook',
+  RegisterGoogle: 'Register dengan Google',
+  RegisterTwitter: 'Register dengan Twitter',
+  RegisterEmail: 'Register dengan Email',
+  SignIn: 'Masuk',
+  BySigningupYouAgree: 'Dengan Mendaftar, Anda menyetujui Aplikasi ini',
+  TermsofService: 'Jangka layanan',
+  Register: 'Daftar',
+  FirstName: 'Nama Depan',
+  LastName: 'Nama Belakang',
+  UserName: 'Nama Pengguna',
+  Email: 'Email',
+  InvalidEmail: 'Email salah',
+  Password: 'Kata Sandi',
+  ConfirmPassword: 'Konfirmasi Kata Sandi',
+  CustomField: 'KolomTambahan',
+  AlreadyUser: 'Nama Pengguna sudah ada',
+  PassdoesntMatch: 'Kata Sandi tidak cocok',
+  InvalidFirstName: 'Nama depan salah',
+  InvalidLastName: 'Nama belakang salah',
+  ByRegisteringYouAgreeTo: 'Dengan mendaftar, Anda setuju untuk',
+  TermsofUse: 'Aturan Penggunaan',
+  And: 'dan',
+  PrivacyPolicy: 'Kebijakan Pribadi',
+  Failed: 'GAGAL',
+  Success: 'SUKSES',
+};
+
+export default register;
