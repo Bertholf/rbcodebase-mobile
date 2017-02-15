53faa532c917997eaeedf3d6889ca88c31f28d4a
putraranji <ogidoank51@yahoo.com>
2017-02-15 18:24:18 +0700

    #356 Refactoring Code

--- app/components/Splash/Walkthrough.js
+++ app/components/Splash/Walkthrough.js
@@ -12,7 +12,7 @@ import {
 } from 'rn-viewpager';
 import { Actions } from 'react-native-router-flux';
 
-const { height, width } = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
 const bromo = require('./../../images/bromo.jpg');
 const everts = require('./../../images/everest.jpg');
@@ -29,7 +29,63 @@ const styles = StyleSheet.create({
     paddingBottom: 50,
   },
   page2: {
-
+    flex: 4,
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20,
+    justifyContent: 'center',
+  },
+  page3: {
+    flex: 1,
+    backgroundColor: '#26C6DA',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingBottom: 50,
+  },
+  page4: {
+    flex: 1,
+    backgroundColor: '#29B6F6',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingBottom: 50,
+  },
+  page5: {
+    flex: 1,
+    backgroundColor: '#64B5F6',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingBottom: 50,
+  },
+  btnContent: {
+    backgroundColor: '#2196F3',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+  },
+  btn: {
+    width,
+    flex: 1,
+    backgroundColor: '#2196F3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnText: {
+    padding: 10,
+    fontWeight: '200',
+    fontSize: 17,
+    color: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    flex: 4,
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20,
+    justifyContent: 'center',
   },
 });
 
@@ -45,66 +101,74 @@ export default class walkthrough extends Component {
       ],
     };
   }
+  renderTitleIndicator() {
+    return <PagerTitleIndicator titles={['one', 'two', 'three', 'four']} />;
+  }
+  renderDotIndicator() {
+    return <PagerDotIndicator pageCount={4} />;
+  }
   render() {
     return (
       <View style={{ backgroundColor: '#2196F3', flex: 1 }}>
-          <IndicatorViewPager
-            style={{ flex: 1 }}
-            indicator={this._renderDotIndicator()}
-          >
-            <View style={styles.page1}>
-              <View style={{flex:5}}>
-                <Image style={{ height: 270,width: width * 1 }} source={this.state.screen[0].imageurl1} />
-              </View>
-              <View style={{flex: 4, flexDirection: 'column', alignItems: 'center', padding: 20, justifyContent: 'center' }}>
-                <Text style={{fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[0].title1}</Text>
-                <Text>{this.state.screen[0].description1}</Text>
-              </View>
+        <IndicatorViewPager
+          style={{ flex: 1 }}
+          indicator={this.renderDotIndicator()}
+        >
+          <View style={styles.page1}>
+            <View style={{ flex: 5 }}>
+              <Image
+                style={{ height: 270, width: width * 1 }} source={this.state.screen[0].imageurl1}
+              />
             </View>
-            <View style={{ flex: 1, backgroundColor:'#26C6DA', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', paddingBottom: 50 }}>
-              <View style={{flex:5}}>
-                <Image style={{ height: 270, width: width * 1 }} source={this.state.screen[1].imageurl2} />
-              </View>
-              <View style={{flex: 4, flexDirection: 'column', alignItems: 'center', padding: 20, justifyContent: 'center' }}>
-                <Text style={{fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[1].title2}</Text>
-                <Text>{this.state.screen[1].description2}</Text>
-              </View>
+            <View style={styles.page2}>
+              <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[0].title1}</Text>
+              <Text>{this.state.screen[0].description1}</Text>
             </View>
-            <View style={{ flex: 1, backgroundColor:'#29B6F6', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', paddingBottom: 50 }}>
-              <View style={{flex:5}}>
-                <Image style={{ height: 270,width: width * 1 }} source={this.state.screen[2].imageurl3} />
-              </View>
-              <View style={{flex: 4, flexDirection: 'column', alignItems: 'center', padding: 20, justifyContent: 'center' }}>
-                <Text style={{fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[2].title3}</Text>
-                <Text>{this.state.screen[2].description3}</Text>
-              </View>
+          </View>
+          <View style={styles.page3}>
+            <View style={{ flex: 5 }}>
+              <Image
+                style={{ height: 270, width: width * 1 }} source={this.state.screen[1].imageurl2}
+              />
             </View>
-            <View style={{ flex: 1, backgroundColor: '#64B5F6', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', paddingBottom: 50 }}>
-              <View style={{flex:5}}>
-                <Image style={{ height: 270,width: width * 1 }} source={this.state.screen[3].imageurl4} />
-              </View>
-            <View style={{flex: 4, flexDirection: 'column', alignItems: 'center', padding: 20, justifyContent: 'center' }}>
-                <Text style={{fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[3].title4}</Text>
-                <Text>{this.state.screen[3].description4}</Text>
+            <View style={styles.page2}>
+              <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[1].title2}</Text>
+              <Text>{this.state.screen[1].description2}</Text>
             </View>
+          </View>
+          <View style={styles.page4}>
+            <View style={{ flex: 5 }}>
+              <Image
+                style={{ height: 270, width: width * 1 }} source={this.state.screen[2].imageurl3}
+              />
             </View>
-          </IndicatorViewPager>
-        <View style={{ backgroundColor: '#2196F3', alignItems: 'center', justifyContent: 'center', height: 50 }}>
+            <View style={styles.content}>
+              <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[2].title3}</Text>
+              <Text>{this.state.screen[2].description3}</Text>
+            </View>
+          </View>
+          <View style={styles.page5}>
+            <View style={{ flex: 5 }}>
+              <Image
+                style={{ height: 270, width: width * 1 }} source={this.state.screen[3].imageurl4}
+              />
+            </View>
+            <View style={styles.content}>
+              <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[3].title4}</Text>
+              <Text>{this.state.screen[3].description4}</Text>
+            </View>
+          </View>
+        </IndicatorViewPager>
+        <View style={styles.btnContent}>
           <TouchableOpacity
             activeOpacity={0.7}
             onPress={Actions.login}
-            style={{ flex: 1, backgroundColor: '#2196F3', width, alignItems: 'center', justifyContent: 'center' }}
+            style={styles.btn}
           >
-            <Text style={{ padding: 10, fontWeight: '200', fontSize: 17, color: 'white', justifyContent: 'center', alignItems: 'center' }}> GET STARTED </Text>
+            <Text style={styles.btnText}> GET STARTED </Text>
           </TouchableOpacity>
         </View>
       </View>
     );
-  }
-  _renderTitleIndicator() {
-    return <PagerTitleIndicator titles={['one', 'two', 'three', 'four']} />;
-  }
-  _renderDotIndicator() {
-    return <PagerDotIndicator pageCount={4} />;
   }
 }
