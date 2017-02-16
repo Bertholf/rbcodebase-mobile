2538496fc688049e7a70838d149833c978385fd7
shilohchis <shidei.chis@gmail.com>
2017-02-16 19:03:25 +0700

    #395: Add Text in the middle of page

--- app/components/Setting/AdPreference.js
+++ app/components/Setting/AdPreference.js
@@ -4,7 +4,11 @@ import { View, Text } from 'react-native';
 export default class AdPref extends Component {
   render() {
     return (
-      <View />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontSize: 18, fontWeight: 'bold', color: '#000' }}>
+          Ad Preference
+        </Text>
+      </View>
     );
   }
 }
