7a8c2631488d260fe64c9907151eee4fb85cd8a1
shilohchis <shidei.chis@gmail.com>
2017-02-20 15:33:01 +0700

    #358: Edit localization in Main Login, Forgot Password, Login by Email Layout, add localization in en and id folder and each index.js

---
+++ app/localizations/id/LoginbyEmail.js
@@ -0,0 +1,10 @@
+const LoginbyEmail = {
+  validate: 'Email atau Kata Sandi tidak sama',
+  validEUser: 'Maaf, username Anda salah',
+  validEPass: 'Kata sandi tidak boleh kosong',
+  pass: 'Kata sandi',
+  signIn: 'Masuk',
+  forgot: 'Lupa kata sandi Anda?',
+};
+
+export default LoginbyEmail;
