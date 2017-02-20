9ccfa13b58ae359053c3daf6e9ba0ba6b815f6f2
putraranji <ogidoank51@yahoo.com>
2017-02-20 16:22:38 +0700

    #356 Change Walkthrough Splash Screen render using map

--- app/components/Splash/Walkthrough.js
+++ app/components/Splash/Walkthrough.js
@@ -8,7 +8,8 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import {
-  IndicatorViewPager, PagerDotIndicator,
+  IndicatorViewPager,
+  PagerDotIndicator,
 } from 'rn-viewpager';
 import { Actions } from 'react-native-router-flux';
 import strings from '../../localizations';
@@ -16,9 +17,9 @@ import strings from '../../localizations';
 const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  page1: {
+  page: {
     flex: 1,
-    backgroundColor: '#26A69A',
+    backgroundColor: '#B3E5FC',
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'center',
@@ -30,30 +31,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
     justifyContent: 'center',
-  },
-  page3: {
-    flex: 1,
-    backgroundColor: '#26C6DA',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingBottom: 50,
-  },
-  page4: {
-    flex: 1,
-    backgroundColor: '#29B6F6',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingBottom: 50,
-  },
-  page5: {
-    flex: 1,
-    backgroundColor: '#64B5F6',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingBottom: 50,
   },
   btnContent: {
     backgroundColor: '#2196F3',
@@ -76,13 +53,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  content: {
-    flex: 4,
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: 20,
-    justifyContent: 'center',
-  },
 });
 
 export default class walkthrough extends Component {
@@ -93,11 +63,23 @@ export default class walkthrough extends Component {
       screen: strings.walkthrought.screen.information,
     };
   }
-  renderTitleIndicator() {
-    return <PagerTitleIndicator titles={['one', 'two', 'three', 'four']} />;
-  }
   renderDotIndicator() {
     return <PagerDotIndicator pageCount={4} />;
+  }
+
+  renderItem(screen){
+    return (
+      <View style={[styles.page, { backgroundColor: screen.bg }]}>
+        <View style={{ flex: 5 }}>
+          <Image
+            style={{ height: 270, width: width * 1 }} source={screen.imageurl}
+          />
+        </View>
+        <View style={styles.page2}>
+          <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{screen.title}</Text>
+          <Text>{screen.description}</Text>
+        </View>
+      </View>);
   }
   render() {
     return (
@@ -106,50 +88,11 @@ export default class walkthrough extends Component {
           style={{ flex: 1 }}
           indicator={this.renderDotIndicator()}
         >
-          <View style={styles.page1}>
-            <View style={{ flex: 5 }}>
-              <Image
-                style={{ height: 270, width: width * 1 }} source={this.state.screen[0].imageurl}
-              />
-            </View>
-            <View style={styles.page2}>
-              <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[0].title}</Text>
-              <Text>{this.state.screen[0].description}</Text>
-            </View>
-          </View>
-          <View style={styles.page3}>
-            <View style={{ flex: 5 }}>
-              <Image
-                style={{ height: 270, width: width * 1 }} source={this.state.screen[1].imageurl}
-              />
-            </View>
-            <View style={styles.page2}>
-              <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[1].title}</Text>
-              <Text>{this.state.screen[1].description}</Text>
-            </View>
-          </View>
-          <View style={styles.page4}>
-            <View style={{ flex: 5 }}>
-              <Image
-                style={{ height: 270, width: width * 1 }} source={this.state.screen[2].imageurl}
-              />
-            </View>
-            <View style={styles.content}>
-              <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[2].title}</Text>
-              <Text>{this.state.screen[2].description}</Text>
-            </View>
-          </View>
-          <View style={styles.page5}>
-            <View style={{ flex: 5 }}>
-              <Image
-                style={{ height: 270, width: width * 1 }} source={this.state.screen[3].imageurl}
-              />
-            </View>
-            <View style={styles.content}>
-              <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 10 }}>{this.state.screen[3].title}</Text>
-              <Text>{this.state.screen[3].description}</Text>
-            </View>
-          </View>
+          {
+        this.state.screen.map((screen) => {
+          return this.renderItem(screen);
+        })
+          }
         </IndicatorViewPager>
         <View style={styles.btnContent}>
           <TouchableOpacity
