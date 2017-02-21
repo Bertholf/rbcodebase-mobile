220aefe891542004f4598750970fc133588b9d01
Rangga Adhitya <deneuv3.4@gmail.com>
2017-02-21 13:35:32 +0700

    #460 add new english localization for edit email

---
+++ app/localizations/en/EditEmail.js
@@ -0,0 +1,9 @@
+const EditEmail = {
+  enterNewEmail: 'Enter Your New Email',
+  invalidEmail: 'Invalid Email Input',
+  sameEmail: 'Seems Your Email is Same as Before',
+  confirmChange: 'Confirm Change',
+  save: 'Save'
+};
+
+module.exports = EditEmail;
