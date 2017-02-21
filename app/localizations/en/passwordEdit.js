be22cbb854b020c8aaf286af032ab7919c7611b2
ahmadahmadi14 <ahmadahmadi140989@gmail.com>
2017-02-21 11:35:39 +0700

    #461 add file password edit indonesia and english language

---
+++ app/localizations/en/passwordEdit.js
@@ -0,0 +1,11 @@
+import LocalizedStrings from 'react-native-localization';
+
+const PassEdit = {
+  enterYourPassword: 'Enter Your Password',
+  inputEnterYourPassword: 'Enter Your old password',
+  enterNewPassword: 'Enter new Password',
+  confirmChange: 'Confirm New Password',
+  save: 'save',
+};
+
+export default PassEdit;
---
+++ app/localizations/id/passwordEdit.js
@@ -0,0 +1,11 @@
+import LocalizedStrings from 'react-native-localization';
+
+const PassEdit = {
+  enterYourPassword: 'Masukkan Kode Sandi Anda',
+  inputEnterYourPassword: 'Masukkan Kode Sandi Lama Anda',
+  enterNewPassword: 'Masukkan Kode Sandi Baru',
+  confirmChange: 'Masukkan Konfirmasi Kode Baru Anda',
+  save: 'Simpan',
+};
+
+export default PassEdit;
