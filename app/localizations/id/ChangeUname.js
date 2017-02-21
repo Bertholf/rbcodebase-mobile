9fecd8c31a2f94e3ef6d91fe6e91acd1a621a544
putraranji <ogidoank51@yahoo.com>
2017-02-21 13:15:13 +0700

    458 Add localizations to ChangeUsername.js file

---
+++ app/localizations/id/ChangeUname.js
@@ -0,0 +1,9 @@
+const changeUsername = {
+  txt1: 'Nama pengguna saat ini',
+  txt2: 'Masukkan nama pengguna baru',
+  txt3: 'Nama pengguna harus unik.',
+  txt4: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  txt5: 'SIMPAN',
+};
+
+export default changeUsername;
