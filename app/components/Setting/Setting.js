2c1d831ef8ef97d9e47ea09642082683d5c89f3e
sutani <sutanighifari@gmail.com>
2017-03-10 15:03:48 +0700

    #630 - update setting birthday

--- app/components/Setting/Setting.js
+++ app/components/Setting/Setting.js
@@ -91,7 +91,8 @@ export default class Setting extends Component {
               </View>
             </View>
           </TouchableOpacity>
-          <TouchableOpacity onPress={Actions.editbirthday({ reRender: () => this.reRender() })}>
+          <TouchableOpacity
+            onPress={() => Actions.editbirthday({ reRender: () => this.reRender() })}
             <View style={styles.list}>
               <View style={{ alignSelf: 'center' }}>
                 <Text style={styles.text}>{strings.settings.birthday}</Text>
