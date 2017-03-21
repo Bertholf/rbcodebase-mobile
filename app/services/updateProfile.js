9ca41d9d77c2a9d81ed1c6187bba563a37b13307
sutani <sutanighifari@gmail.com>
2017-03-21 19:51:17 +0700

    #704 - update name display

--- app/services/updateProfile.js
+++ app/services/updateProfile.js
@@ -1,7 +1,7 @@
 import auth from './auth';
 
-const saveProfile = (id, name_first, name_last, name_slug, gender, phone, date_birth, password, password_confirmation) => {
-  auth.updateProfile(id, name_first, name_last, name_slug, gender, phone, date_birth, password, password_confirmation)
+const saveProfile = (id, name_first, name_last, name_display,name_slug, gender, phone, date_birth, password, password_confirmation) => {
+  auth.updateProfile(id, name_first, name_last, name_display,name_slug, gender, phone, date_birth, password, password_confirmation)
   .then((response) => {
     console.log('update===', response);
   }).catch(err => console.log('===ERROR SAVE===', err));
