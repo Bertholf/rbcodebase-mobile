84773c042736f6e9251e4c5a7ef7341355c8b835
tyokusuma <tyoaris21@gmail.com>
2017-04-03 12:20:47 +0700

    741 redux disable action

---
+++ app/containers/networkContainer.js
@@ -0,0 +1,25 @@
+import { connect } from 'react-redux';
+import { getNetworkStatus, changeNetworkStatus, _handleConnectivityChange } from '../actions/networkStatus';
+import NameEdit from  '../components/Setting/ChangeName';
+import setting from  '../components/Setting/setting';
+
+const mapStateToProps = ({ app }) => ({
+  network: app.networkState.network,
+  change: app.networkState.network,
+  handler: app.networkState.network,
+});
+
+const mapDispatchToProps = dispatch => ({
+  networkAction: () => {
+    // return true;
+    dispatch(getNetworkStatus());
+  },
+  changeNetworkAction: () => {
+    dispatch(changeNetworkStatus());
+  },
+  handler: () => {
+    dispatch(_handleConnectivityChange());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NameEdit);
