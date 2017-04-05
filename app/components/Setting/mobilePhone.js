cd536af31cbbdaa82838e5f0d2cc9531bdf8d754
sutani <sutanighifari@gmail.com>
2017-04-05 15:26:48 +0700

    add styles global edit mobile phone

--- app/components/Setting/mobilePhone.js
+++ app/components/Setting/mobilePhone.js
@@ -15,51 +15,7 @@ import saveProfile from '../../services/updateProfile';
 import auth from './../../services/auth';
 import IconClose from './../../layouts/IconClose';
 import strings from '../../localizations';
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    flex: 1,
-    marginTop: 5,
-  },
-  textinputWrapperStyle: {
-    borderColor: '#2196F3',
-    borderWidth: 0.8,
-    borderRadius: 2,
-    flexDirection: 'column',
-    paddingLeft: 4,
-    paddingRight: 4,
-    marginBottom: 10,
-    height: 55,
-  },
-  textinputStyle: {
-    height: 55,
-  },
-  loginInput: {
-    height: 50,
-    marginTop: 10,
-    padding: 4,
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: '#48BBEC',
-    borderRadius: 0,
-    color: '#48BBEC',
-  },
-  button: {
-    backgroundColor: '#2196F3',
-    borderRadius: 2,
-    elevation: 2,
-    paddingTop: 5,
-    paddingBottom: 5,
-    marginTop: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 24,
-  },
-});
+import styles from '../../style/StyleGlobal';
 
 export default class MobilePhone extends Component {
   constructor(props) {
--- app/style/StyleGlobal.js
+++ app/style/StyleGlobal.js
@@ -127,8 +127,54 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: '#f2003d',
   },
-
   // --------end-------------
+
+  // -------styles Mobile Phone ------------
+  container: {
+    padding: 16,
+    flex: 1,
+    marginTop: 5,
+  },
+  textinputWrapperStyle: {
+    borderColor: '#2196F3',
+    borderWidth: 0.8,
+    borderRadius: 2,
+    flexDirection: 'column',
+    paddingLeft: 4,
+    paddingRight: 4,
+    marginBottom: 10,
+    height: 55,
+  },
+  textinputStyle: {
+    height: 55,
+  },
+  loginInput: {
+    height: 50,
+    marginTop: 10,
+    padding: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 0,
+    color: '#48BBEC',
+  },
+  button: {
+    backgroundColor: '#2196F3',
+    borderRadius: 2,
+    elevation: 2,
+    paddingTop: 5,
+    paddingBottom: 5,
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 24,
+  },
+  
+// --------------------end---------------
+
   logoGoogle: {
     height: 23,
     width: 35,
