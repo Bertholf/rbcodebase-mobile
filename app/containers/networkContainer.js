f92519956567ca467e2b8f1baa6f82feb53ac6c4
Rangga Adhitya <deneuv3.4@gmail.com>
2017-04-05 18:24:24 +0700

    #741 - restore from revert action

---
+++ app/containers/networkContainer.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import { getNetworkStatus, changeNetworkStatus, _handleConnectivityChange } from '../actions/networkStatus';
+import NameEdit from  '../components/Setting/ChangeName';
+
+const mapStateToProps = ({ app }) => ({
+  network: app.networkState.network,
+  change: app.networkState.network,
+  handler: app.networkState.network,
+});
+
+const mapDispatchToProps = dispatch => ({
+  networkAction: () => {
+    // return true;
+    dispatch(getNetworkStatus());
+  },
+  changeNetworkAction: () => {
+    dispatch(changeNetworkStatus());
+  },
+  handler: () => {
+    dispatch(_handleConnectivityChange());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NameEdit);
