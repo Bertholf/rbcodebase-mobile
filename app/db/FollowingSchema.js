2f637045addd03db5a1859a4b99c661eae9d723b
Rangga Adhitya Prawira <deneuv3.4@gmail.com>
2017-04-10 15:22:03 +0700

    #769 - Fix invalid schema creation

--- app/db/FollowingSchema.js
+++ app/db/FollowingSchema.js
@@ -1,4 +1,6 @@
 import SQLite from 'react-native-sqlite-storage';
+import { AsyncStorage } from 'react-native';
+import follows from '../services/follows';
 
 SQLite.DEBUG(true);
 SQLite.enablePromise(true);
@@ -10,6 +12,7 @@ const dbSize = 200000;
 let db;
 
 const saveLeader = (tx, values) => {
+  console.log('GO TO saveleader');
   const leader = values.leader;
   tx.executeSql('INSERT INTO Leader '
       + 'VALUES ('
@@ -42,6 +45,7 @@ const saveLeader = (tx, values) => {
 };
 
 const saveFollower = (tx, values) => {
+  console.log('GO TO savefollower');
   const follower = values.follower;
   tx.executeSql('INSERT INTO Follower '
       + 'VALUES ('
@@ -74,6 +78,7 @@ const saveFollower = (tx, values) => {
 };
 
 const saveRelation = (tx, values) => {
+  console.log('GO TO SAVE RELATION');
   const data = values;
   tx.executeSql('INSERT INTO Following (id, leader_id, follower_id, status, created_at, updated_at, deleted_at) '
       + 'VALUES ('
@@ -86,6 +91,7 @@ const saveRelation = (tx, values) => {
 };
 
 const closedatabase = () => {
+  console.log('GO to CLOSE DB');
   if (db) {
     console.log('Close database');
     db.close().then(() => console.log('database closed'));
@@ -94,28 +100,47 @@ const closedatabase = () => {
   }
 };
 
-const mapFollowerToDatabase = (tx, res) => {
-  const data = res;
-  data.map(value => saveFollower(tx, value));
-  data.map(value => saveLeader(tx, value));
-  data.map(value => saveRelation(tx, value));
+const mapFollowerToDatabase = (tx) => {
+  let data = null;
+  console.log('MAP VALUE');
+  AsyncStorage.getItem('userId')
+    .then((myId) => {
+      follows.searchFollowing(this.state.name, myId)
+      .then((res) => {
+        data = res;
+        data.map(value => saveFollower(tx, value));
+        data.map(value => saveLeader(tx, value));
+        data.map(value => saveRelation(tx, value));
+      })
+      .catch(err => this.showError(err));
+    })
+    .catch();
 };
 
 const createSchemaDb = (tx, res) => {
+  console.log('CREATE SCHEMA');
+  tx.executeSql('DROP TABLE IF EXISTS Version;');
   tx.executeSql('DROP TABLE IF EXISTS Relation;');
   tx.executeSql('DROP TABLE IF EXISTS Follower;');
   tx.executeSql('DROP TABLE IF EXISTS Leader;');
   tx.executeSql('DROP TABLE IF EXISTS Setting;');
 
+  tx.executeSql('CREATE TABLE IF NOT EXISTS Version( '
+    + 'version_id INTEGER PRIMARY KEY NOT NULL); ').catch((error) => {
+      console.log('ERROR FOUND, ', error);
+    });
+
   // Following Table
   tx.executeSql('CREATE TABLE IF NOT EXISTS Following( '
     + 'id INTEGER PRIMARY KEY NOT NULL, '
-    + 'FOREIGN KEYS (leader_id) REFERENCE Leader (id), '
-    + 'FOREIGN KEYS (follower_id) REFERENCE Follower (id), '
+    + 'leader_id INTEGER, '
+    + 'follower_id INTEGER, '
     + 'status VARCHAR, '
     + 'created_at DATE, '
     + 'updated_at DATE, '
     + 'deleted_at DATE, '
+    + 'FOREIGN KEY ( leader_id ) REFERENCES Leader ( id ) '
+    + 'FOREIGN KEY ( follower_id ) REFERENCES Follower ( id )'
     + '); ').catch(error => console.log('ERROR CREATE DB', error));
 
   // Follower table schema
@@ -124,30 +149,31 @@ const createSchemaDb = (tx, res) => {
     + 'name_first VARCHAR(30), '
     + 'name_last VARCHAR(30), '
     + 'name_display VARCHAR(50), '
-    + 'name_slug VARCHAR(20) '
-    + 'email VARCHAR(50) '
-    + 'cell_number VARCHAR(20) '
-    + 'cell_carrier VARCHAR(30) '
-    + 'status INTEGER '
-    + 'confirmation_code VARCHAR '
-    + 'confirmed INTEGER '
-    + 'verified INTEGER '
-    + 'language VARCHAR(4) '
-    + 'timezone VARCHAR(10) '
-    + 'date_birth DATE '
-    + 'timeline_id INTEGER '
-    + 'img_avatar VARCHAR '
-    + 'img_background VARCHAR '
-    + 'referring_user_id VARCHAR '
+    + 'name_slug VARCHAR(20), '
+    + 'email VARCHAR(50), '
+    + 'cell_number VARCHAR(20), '
+    + 'cell_carrier VARCHAR(30), '
+    + 'status INTEGER, '
+    + 'confirmation_code VARCHAR, '
+    + 'confirmed INTEGER, '
+    + 'verified INTEGER, '
+    + 'language VARCHAR(4), '
+    + 'timezone VARCHAR(10), '
+    + 'date_birth DATE, '
+    + 'timeline_id INTEGER, '
+    + 'img_avatar VARCHAR, '
+    + 'img_background VARCHAR, '
+    + 'referring_user_id VARCHAR, '
     + 'created_at DATE, '
     + 'updated_at DATE, '
     + 'deleted_at DATE, '
-    + 'current_team_id INTEGER'
-    + 'gender VARCHAR(10) '
-    + 'picture VARCHAR '
-    + 'access_token VARCHAR '
-    + 'registered BOOLEAN'
-    + 'FOREIGN KEY (setting) REFERENCE Setting (id) '
+    + 'current_team_id INTEGER, '
+    + 'gender VARCHAR(10), '
+    + 'picture VARCHAR, '
+    + 'access_token VARCHAR, '
+    + 'registered BOOLEAN, '
+    + 'setting INTEGER, '
+    + 'FOREIGN KEY ( setting ) REFERENCES Setting ( id ) '
     + '); ').catch(error => console.log('ERROR CREATE DB', error));
 
   // leader table schema
@@ -156,58 +182,60 @@ const createSchemaDb = (tx, res) => {
     + 'name_first VARCHAR(30), '
     + 'name_last VARCHAR(30), '
     + 'name_display VARCHAR(50), '
-    + 'name_slug VARCHAR(20) '
-    + 'email VARCHAR(50) '
-    + 'cell_number VARCHAR(20) '
-    + 'cell_carrier VARCHAR(30) '
-    + 'status INTEGER '
-    + 'confirmation_code VARCHAR '
-    + 'confirmed INTEGER '
-    + 'verified INTEGER '
-    + 'language VARCHAR(4) '
-    + 'timezone VARCHAR(10) '
-    + 'date_birth DATE '
-    + 'timeline_id INTEGER '
-    + 'img_avatar VARCHAR '
-    + 'img_background VARCHAR '
-    + 'referring_user_id VARCHAR '
+    + 'name_slug VARCHAR(20), '
+    + 'email VARCHAR(50), '
+    + 'cell_number VARCHAR(20), '
+    + 'cell_carrier VARCHAR(30) ,'
+    + 'status INTEGER, '
+    + 'confirmation_code VARCHAR, '
+    + 'confirmed INTEGER, '
+    + 'verified INTEGER, '
+    + 'language VARCHAR(4), '
+    + 'timezone VARCHAR(10), '
+    + 'date_birth DATE,'
+    + 'timeline_id INTEGER, '
+    + 'img_avatar VARCHAR, '
+    + 'img_background VARCHAR, '
+    + 'referring_user_id VARCHAR, '
     + 'created_at DATE, '
     + 'updated_at DATE, '
     + 'deleted_at DATE, '
-    + 'current_team_id INTEGER'
-    + 'gender VARCHAR(10) '
-    + 'picture VARCHAR '
-    + 'access_token VARCHAR '
-    + 'registered BOOLEAN'
-    + 'FOREIGN KEY (setting) REFERENCE Setting (id) '
+    + 'current_team_id INTEGER, '
+    + 'gender VARCHAR(10), '
+    + 'picture VARCHAR, '
+    + 'access_token VARCHAR, '
+    + 'registered BOOLEAN, '
+    + 'setting INTEGER, '
+    + 'FOREIGN KEY ( setting ) REFERENCES Setting ( id ) '
     + '); ').catch(error => console.log('ERROR CREATE DB', error));
 
   // Setting table schema
   tx.executeSql('CREATE TABLE IF NOT EXISTS Follower( '
     + 'id INTEGER PRIMARY KEY NOT NULL, '
     + 'user_id INTEGER, '
-    + 'privacy_follow VARCHAR(20) '
-    + 'privacy_follow_confirm VARCHAR(20) '
-    + 'privacy_follow VARCHAR(20) '
-    + 'privacy_comment VARCHAR(20) '
-    + 'privacy_post VARCHAR(20) '
-    + 'privacy_timeline_post VARCHAR(20) '
-    + 'privacy_message VARCHAR(20) '
-    + 'email_follow INT '
-    + 'email_post_like INT '
-    + 'email_post_share INT '
-    + 'email_comment_post INT '
-    + 'email_comment_like INT '
-    + 'email_comment_reply INT '
+    + 'privacy_follow VARCHAR(20), '
+    + 'privacy_follow_confirm VARCHAR(20), '
+    + 'privacy_follow VARCHAR(20), '
+    + 'privacy_comment VARCHAR(20), '
+    + 'privacy_post VARCHAR(20), '
+    + 'privacy_timeline_post VARCHAR(20), '
+    + 'privacy_message VARCHAR(20), '
+    + 'email_follow INT, '
+    + 'email_post_like INT, '
+    + 'email_post_share INT, '
+    + 'email_comment_post INT, '
+    + 'email_comment_like INT, '
+    + 'email_comment_reply INT, '
     + 'created_at DATE, '
     + 'updated_at DATE, '
-    + 'deleted_at DATE, '
+    + 'deleted_at DATE '
     + '); ').catch(error => console.log('ERROR CREATE DB', error));
 
   mapFollowerToDatabase(tx, res);
 };
 
 const queryfollowing = (tx) => {
+  console.log('QUEWRY FOLLOWING');
   tx.executeSql('SELECT * FROM Following').then(([tx, result]) => {
     const len = result.rows.length;
     for (let i = 0; i < len; i++) {
@@ -218,6 +246,7 @@ const queryfollowing = (tx) => {
 };
 
 const populateDatabase = () => {
+  console.log('POPULATE DATABASE');
   db.executeSql('SELECT 1 FROM Version LIMIT 1').then(() => {
     db.transaction(queryfollowing).then(() => {});
   }).catch((error) => {
@@ -229,11 +258,12 @@ const populateDatabase = () => {
 };
 
 const loadAndQuery = () => {
+  console.log('LOAD QUERY DATABASE');
   SQLite.echoTest().then(() => {
     SQLite.openDatabase(dbName, dbVersion, dbDisplayName, -1).then((DB) => {
       db = DB;
       populateDatabase(DB);
-    }).catch(error => console.log('ERror', error));
+    }).catch(error => console.log('ERror to load query', error));
   }).catch(err => console.log('plugin Error', err));
 };
 
