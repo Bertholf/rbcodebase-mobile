3eb55cdfb88fd452805b722c8c056d5e0e744ca5
tyokusuma <tyoaris21@gmail.com>
2017-04-13 01:33:09 +0700

    style globa component changepassword

--- app/style/StyleGlobal.js
+++ app/style/StyleGlobal.js
@@ -441,5 +441,39 @@ const styles = StyleSheet.create({
     marginTop: 6,
     height: 40,
   },
+  box: {
+    marginTop: 70,
+    width: width * 0.9,
+  },
+  title: {
+    alignItems: 'center',
+    top: 20,
+    marginBottom: 10,
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+  },
+  textchangPass: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#2196F3',
+  },
+  new_password: {
+    marginTop: 20,
+    color: '#2196f3',
+  },
+  button_save: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    marginTop: 50,
+    borderRadius: 5,
+    alignItems: 'center',
+    width: width * 0.5,
+  },
+  middle_line: {
+    borderWidth: 0.4,
+    borderColor: 'grey',
+    width: width * 0.2,
+    height: 0.5,
+  },
 });
 export default styles;
