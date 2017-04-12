5fb3e008439bc7afb8a2b3cbc5dcfddc1bccb664
tyokusuma <tyoaris21@gmail.com>
2017-04-13 03:25:26 +0700

    global style on followeer component

--- app/components/Profile/Follower.js
+++ app/components/Profile/Follower.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Alert, ListView, ActivityIndicator, AsyncStorage, Text, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Alert,
+  ListView,
+  ActivityIndicator,
+  AsyncStorage,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { Container, Item, Icon, Input } from 'native-base';
 import follows from '../../services/follows';
 import ListFollow from './ListFollow';
 import strings from '../../localizations';
+import styles from './../../style/StyleGlobal';
 
 const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 export default class FollowingMe extends React.Component {
@@ -29,20 +38,22 @@ export default class FollowingMe extends React.Component {
     */
     if (typeof this.props.user_id === 'undefined') {
       AsyncStorage.getItem('userId')
-      .then((myId) => {
-        follows.searchFollower(this.state.name, myId)
+        .then((myId) => {
+          follows
+            .searchFollower(this.state.name, myId)
+            .then((res) => {
+              this.changeState(res);
+            })
+            .catch(err => this.showError(err));
+        })
+        .catch();
+    } else {
+      follows
+        .showFollower(this.props.user_id)
         .then((res) => {
           this.changeState(res);
         })
         .catch(err => this.showError(err));
-      })
-      .catch();
-    } else {
-      follows.showFollower(this.props.user_id)
-      .then((res) => {
-        this.changeState(res);
-      })
-      .catch(err => this.showError(err));
     }
   }
 
@@ -74,22 +85,28 @@ export default class FollowingMe extends React.Component {
     AsyncStorage.getItem('userId')
       .then((myId) => {
         if (val !== '') {
-          this.setState({ name: val, wait: true, someone: `${strings.listfollow.named} "${val}"`, search: 1 });
+          this.setState({
+            name: val,
+            wait: true,
+            someone: `${strings.listfollow.named} "${val}"`,
+            search: 1,
+          });
         } else {
           this.setState({ name: val, wait: true, someone: '', search: 0 });
         }
-        follows.searchFollower(this.state.name, myId)
-        .then((res) => {
-          this.changeState(res);
-        })
-        .catch((thrown) => {
-          this.setState({ requesting: false });
-          if (follows.client().isCancel(thrown)) {
-            console.log('Request is canceled', thrown.message);
-          } else {
-            console.log('ERROR', thrown);
-          }
-        });
+        follows
+          .searchFollower(this.state.name, myId)
+          .then((res) => {
+            this.changeState(res);
+          })
+          .catch((thrown) => {
+            this.setState({ requesting: false });
+            if (follows.client().isCancel(thrown)) {
+              console.log('Request is canceled', thrown.message);
+            } else {
+              console.log('ERROR', thrown);
+            }
+          });
       })
       .catch(err => console.log(err));
   }
@@ -99,7 +116,6 @@ export default class FollowingMe extends React.Component {
     this.setState({ wait: true });
     this.timer = setTimeout(() => this.searchUpdate(value), 1250);
   }
-
 
   render() {
     // show content in component if no data or has data
@@ -121,11 +137,12 @@ export default class FollowingMe extends React.Component {
             *Loading if data in requesting
           */}
 
-          {this.state.wait ? <ActivityIndicator size={'large'} style={{ marginTop: 40 }} /> :
-          <ListView
-            dataSource={ds.cloneWithRows(this.state.follower)}
-            renderRow={rowData => <ListFollow rowData={{ ...rowData, type: 'follower' }} />}
-          />}
+          {this.state.wait
+            ? <ActivityIndicator size={'large'} style={{ marginTop: 40 }} />
+            : <ListView
+              dataSource={ds.cloneWithRows(this.state.follower)}
+              renderRow={rowData => <ListFollow rowData={{ ...rowData, type: 'follower' }} />}
+            />}
 
         </Container>
       );
@@ -133,38 +150,47 @@ export default class FollowingMe extends React.Component {
       // Return this View if there is no Data Showed
       return (
         // Search Bar
-        <Container>
-          <Item>
-            <Icon name="search" />
-            <Input
-              placeholder={strings.listfollow.searchFollower}
-              onChangeText={value => this.cancelRequest(value)}
-            />
-          </Item>
+        (
+          <Container>
+            <Item>
+              <Icon name="search" />
+              <Input
+                placeholder={strings.listfollow.searchFollower}
+                onChangeText={value => this.cancelRequest(value)}
+              />
+            </Item>
 
-          {this.state.wait ? <ActivityIndicator size={'large'} style={{ marginTop: 40 }} /> :
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
-            {/*
+            {this.state.wait
+              ? <ActivityIndicator size={'large'} style={{ marginTop: 40 }} />
+              : <View style={styles.viewWait}>
+                {/*
                 Should show different message when user has no one following
                 and didn't have following named!!
             */}
-            {this.state.someone !== '' && this.state.search === 1 ?
-              <Text numberOfLines={2} style={{ color: '#000', fontSize: 15, alignItems: 'center' }}>{strings.listfollow.nofollower} {this.state.someone}</Text>
-            : <Text style={{ color: '#000', fontSize: 15, alignItems: 'center' }}>{strings.listfollow.nofollower}</Text>}
+                {this.state.someone !== '' && this.state.search === 1
+                    ? <Text numberOfLines={2} style={styles.textWait}>
+                      {strings.listfollow.nofollower} {this.state.someone}
+                    </Text>
+                    : <Text style={styles.textWait}>
+                      {strings.listfollow.nofollower}
+                    </Text>}
 
-            <TouchableOpacity
-              onPress={() => Actions.addfriendscreen()}
-              style={{ borderRadius: 6, alignItems: 'center', justifyContent: 'center', backgroundColor: '#313bf9', margin: 10, padding: 10, height: 50, width: 120 }}
-            >
-              <Text style={{ color: '#fff', textAlign: 'center' }}>{strings.listfollow.findfriend}</Text>
-            </TouchableOpacity>
-          </View>}
+                <TouchableOpacity
+                  onPress={() => Actions.addfriendscreen()}
+                  style={styles.onButtonSearchFriend}
+                >
+                  <Text style={styles.textWait}>
+                    {strings.listfollow.findfriend}
+                  </Text>
+                </TouchableOpacity>
+              </View>}
 
-        </Container>
+          </Container>
+        )
       );
     }
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.Indicator}>
         <ActivityIndicator size={'large'} />
       </View>
     );
