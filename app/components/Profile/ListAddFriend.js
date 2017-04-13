defcdffc515adf950987ea7af1b0ee638555eaaa
tyokusuma <tyoaris21@gmail.com>
2017-04-13 11:32:21 +0700

    style global list follow and unfollow

--- app/components/Profile/ListAddFriend.js
+++ app/components/Profile/ListAddFriend.js
@@ -2,64 +2,7 @@ import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
 import { View, Alert, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 import strings from '../../localizations';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    paddingTop: 5,
-    paddingLeft: 10,
-    paddingRight: 10,
-    backgroundColor: '#ffffff',
-    borderColor: '#9E9E9E',
-    borderWidth: 0.3,
-  },
-  account: {
-    paddingLeft: 10,
-  },
-  user: {
-    marginLeft: 3,
-    fontSize: 12,
-    fontWeight: 'bold',
-    color: 'black',
-    marginTop: 10,
-  },
-  photo: {
-    width: 40,
-    height: 40,
-    marginLeft: 2,
-    marginTop: 6,
-    marginBottom: 6,
-    borderRadius: 50,
-  },
-  detail: {
-    fontSize: 11,
-    color: 'grey',
-    marginRight: 5,
-    marginLeft: 3,
-  },
-  buttonFollow: {
-    flexDirection: 'row',
-    padding: 6,
-    backgroundColor: '#2196F3',
-    justifyContent: 'space-between',
-    color: 'white',
-    alignItems: 'center',
-    marginTop: 7,
-    borderRadius: 2,
-  },
-  buttonUnfollow: {
-    flexDirection: 'row',
-    padding: 6,
-    backgroundColor: '#ddd',
-    justifyContent: 'space-between',
-    color: '#333',
-    alignItems: 'center',
-    marginTop: 7,
-    borderRadius: 2,
-  },
-});
+import styles from './../../style/StyleGlobal';
 
 export default class ListAddFriend extends Component {
   constructor(props) {
@@ -73,11 +16,16 @@ export default class ListAddFriend extends Component {
 
   toggleSwitch() {
     if (!this.state.clicked) {
-      Alert.alert(strings.addfriend.cofirmation,
-               strings.addfriend.question, [
-                { text: strings.addfriend.cancel, onPress: () => this.setState({ clicked: this.state.clicked }) },
-                { text: strings.addfriend.yes, onPress: () => this.setState({ clicked: !this.state.clicked }) },
-               ]);
+      Alert.alert(strings.addfriend.cofirmation, strings.addfriend.question, [
+        {
+          text: strings.addfriend.cancel,
+          onPress: () => this.setState({ clicked: this.state.clicked }),
+        },
+        {
+          text: strings.addfriend.yes,
+          onPress: () => this.setState({ clicked: !this.state.clicked }),
+        },
+      ]);
     } else {
       this.setState({ clicked: !this.state.clicked });
     }
@@ -86,30 +34,29 @@ export default class ListAddFriend extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => Actions.profile({ profile: this.props.rowData })}
-        activeOpacity={0.7}
-      >
-        <View >
-          <View style={{ flexDirection: 'row' }}>
-            <Image source={{ uri: this.props.rowData.picture }} style={styles.photo} />
-            <View style={styles.account}>
-              <Text style={styles.user}>
-                {this.props.rowData.name_first} {this.props.rowData.name_last}
-              </Text>
-              <Text style={styles.detail}>{this.props.rowData.name_slug}</Text>
+        <TouchableOpacity
+          onPress={() => Actions.profile({ profile: this.props.rowData })}
+          activeOpacity={0.7}
+        >
+          <View>
+            <View style={{ flexDirection: 'row' }}>
+              <Image source={{ uri: this.props.rowData.picture }} style={styles.photo} />
+              <View style={styles.account}>
+                <Text style={styles.user}>
+                  {this.props.rowData.name_first} {this.props.rowData.name_last}
+                </Text>
+                <Text style={styles.detail}>{this.props.rowData.name_slug}</Text>
+              </View>
             </View>
-          </View>
 
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity  onPress={() => this.toggleSwitch()}>
-        <Text style={this.state.clicked ? styles.buttonFollow : styles.buttonUnfollow}>
-          {this.state.clicked ? strings.addfriend.add : strings.addfriend.unfriend }
-        </Text>
-      </TouchableOpacity>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => this.toggleSwitch()}>
+          <Text style={this.state.clicked ? styles.buttonFollow : styles.buttonUnfollow}>
+            {this.state.clicked ? strings.addfriend.add : strings.addfriend.unfriend}
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
-
 }
--- app/components/Profile/ListApproval.js
+++ app/components/Profile/ListApproval.js
@@ -1,67 +1,19 @@
 import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
-import { View, Alert, StyleSheet, Text, TouchableOpacity, Image, AsyncStorage, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Alert,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+  AsyncStorage,
+  ActivityIndicator,
+} from 'react-native';
 import { Content, ListItem, Body, Right, Container } from 'native-base';
 import strings from '../../localizations';
 import follows from '../../services/follows';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    paddingTop: 5,
-    paddingLeft: 10,
-    paddingRight: 10,
-    backgroundColor: '#ffffff',
-    borderColor: '#9E9E9E',
-    borderWidth: 0.3,
-  },
-  account: {
-    paddingLeft: 10,
-  },
-  user: {
-    marginLeft: 3,
-    fontSize: 12,
-    fontWeight: 'bold',
-    color: 'black',
-    marginTop: 10,
-  },
-  photo: {
-    width: 40,
-    height: 40,
-    marginLeft: 2,
-    marginTop: 6,
-    marginBottom: 6,
-    borderRadius: 40 / 2,
-  },
-  detail: {
-    fontSize: 11,
-    color: 'grey',
-    marginRight: 5,
-    marginLeft: 3,
-  },
-  buttonFollow: {
-    flexDirection: 'row',
-    padding: 6,
-    backgroundColor: '#2196F3',
-    justifyContent: 'space-between',
-    color: 'white',
-    alignItems: 'center',
-    marginTop: 7,
-    borderRadius: 2,
-  },
-  buttonUnfollow: {
-    flexDirection: 'row',
-    padding: 6,
-    backgroundColor: '#ddd',
-    justifyContent: 'space-between',
-    color: '#333',
-    alignItems: 'center',
-    marginTop: 7,
-    borderRadius: 2,
-  },
-});
+import styles from './../../style/StyleGlobal';
 
 export default class ListFollow extends Component {
   constructor(props) {
@@ -77,11 +29,12 @@ export default class ListFollow extends Component {
     AsyncStorage.getItem('userId')
       .then((myId) => {
         console.log('This is My ID=================', myId);
-        follows.showApproval(myId)
-        .then((res) => {
-          console.log('DI SINI');
-        })
-        .catch(err => console.log('Error!!!!', err));
+        follows
+          .showApproval(myId)
+          .then((res) => {
+            console.log('DI SINI');
+          })
+          .catch(err => console.log('Error!!!!', err));
       })
       .catch(err => console.log('fail to get user id from asyncStorege', err));
   }
@@ -95,13 +48,23 @@ export default class ListFollow extends Component {
       const idFol = this.props.rowData.follower_id;
       const idLed = this.props.rowData.leader_id;
       const status = 'approved';
-      follows.showFollowing2(idFol, idLed)
+      follows
+        .showFollowing2(idFol, idLed)
         .then((resp) => {
-          console.log('DAPETIN NO ID', resp.data.id); follows.reqApproval(idFol, idLed, status, resp.data.id)
-          .then((resp) => { console.log('RESPON AFTER APPROVE', resp); this.props.rowData.rerender(); })
-          .catch((err) => { console.log('ERROR', err); });
+          console.log('DAPETIN NO ID', resp.data.id);
+          follows
+            .reqApproval(idFol, idLed, status, resp.data.id)
+            .then((resp) => {
+              console.log('RESPON AFTER APPROVE', resp);
+              this.props.rowData.rerender();
+            })
+            .catch((err) => {
+              console.log('ERROR', err);
+            });
         })
-        .catch((err) => { console.log('Error', err); });
+        .catch((err) => {
+          console.log('Error', err);
+        });
     }
   }
 
@@ -138,14 +101,18 @@ export default class ListFollow extends Component {
             </Body>
           </TouchableOpacity>
           <Right>
-            <TouchableOpacity onPress={() => { this.approve(); }}>
+            <TouchableOpacity
+              onPress={() => {
+                this.approve();
+              }}
+            >
               <Text style={styles.buttonFollow}>
-                {strings.listfollow.approve}</Text>
+                {strings.listfollow.approve}
+              </Text>
             </TouchableOpacity>
           </Right>
         </ListItem>
       </Container>
     );
   }
-
 }
