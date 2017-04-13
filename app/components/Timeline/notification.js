0a859b4fdeb7cde1ea5bc839948bfe8624868b9a
sutani <sutanighifari@gmail.com>
2017-04-13 17:07:38 +0700

    clear-code-component

--- app/components/Timeline/notification.js
+++ app/components/Timeline/notification.js
@@ -54,11 +54,6 @@ export default class Notification extends React.Component {
   }
 
   componentDidMount() {
-    // notifService.getNotifications()
-    // .then((data) => {
-    //   this.setState({ notif: data, loading: false });
-    //   console.log('hello message', this.state.notif);
-    // }).catch(err => console.log(err));
   }
 
   render() {
