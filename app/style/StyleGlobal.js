50c56bac28d5f7e3b807f75848962532896cd4e5
akbar <akbar_rachman@ymail.com>
2017-06-02 22:04:27 +0700

    #853 - Display on login screen

--- app/style/StyleGlobal.js
+++ app/style/StyleGlobal.js
@@ -3,7 +3,7 @@ import { StyleSheet, Dimensions, Platform } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  container: {
+  containers: {
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#fff',
